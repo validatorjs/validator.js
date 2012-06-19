bfe5d27eae3431c0c4e778a846a69be02fc282d0
Chris O'Hara <cohara87@gmail.com>
2012-06-19 22:05:11 +1000

    Fix a global leak, re #88

--- validator.js
+++ validator.js
@@ -695,6 +695,7 @@
 
     //Thanks to github.com/sreuter for the idea.
     Validator.prototype.isUUID = function(version) {
+        var pattern;
         if (version == 3 || version == 'v3') {
             pattern = /[0-9A-F]{8}-[0-9A-F]{4}-3[0-9A-F]{3}-[0-9A-F]{4}-[0-9A-F]{12}$/i;
         } else if (version == 4 || version == 'v4') {
