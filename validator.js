953559b7762e7ea786fd2a601204fea66981813c
Chris O'Hara <cohara87@gmail.com>
2014-01-31 08:19:41 +1100

    Don't coerce to a string if already a string, re #253

--- validator.js
+++ validator.js
@@ -67,8 +67,10 @@
             input = '';
         } else if (typeof input === 'object' && input.toString) {
             input = input.toString();
+        } else if (typeof input !== 'string') {
+            input += '';
         }
-        return input + '';
+        return input;
     };
 
     validator.toDate = function (date) {
