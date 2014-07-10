7cc6b423da99f1f1f0268e6bd5961e1fcd5a6915
Chris O'Hara <cohara87@gmail.com>
2014-07-10 22:07:27 +0700

    Escape apostrophes, re #296

--- validator.js
+++ validator.js
@@ -380,6 +380,7 @@
     validator.escape = function (str) {
         return (str.replace(/&/g, '&amp;')
             .replace(/"/g, '&quot;')
+            .replace(/'/g, '&apos;')
             .replace(/</g, '&lt;')
             .replace(/>/g, '&gt;'));
     };
