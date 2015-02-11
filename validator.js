a4ff13c22a2281bd0099f5bd97860a14d349e3f9
Chris O'Hara <cohara87@gmail.com>
2015-02-12 06:59:36 +0700

    Ensure the min version contains only ascii

--- validator.js
+++ validator.js
@@ -526,7 +526,7 @@
     };
 
     validator.stripLow = function (str, keep_new_lines) {
-        var chars = keep_new_lines ? '\x00-\x09\x0B\x0C\x0E-\x1F\x7F' : '\x00-\x1F\x7F';
+        var chars = keep_new_lines ? '\\x00-\\x09\\x0B\\x0C\\x0E-\\x1F\\x7F' : '\\x00-\\x1F\\x7F';
         return validator.blacklist(str, chars);
     };
 
