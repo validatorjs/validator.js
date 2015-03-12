a26584ba8a019c0316e7dd10f0a3bad04a9c8a4d
Hauke Henningsen <sqrt@entless.org>
2015-03-12 02:41:45 +0100

    Add validator check for ISINs

--- test/validators.js
+++ test/validators.js
@@ -785,6 +785,27 @@ describe('Validators', function () {
             ]
         });
     });
+    
+    it('should validate ISINs', function () {
+        test({
+            validator: 'isISIN'
+          , valid: [
+                'AU0000XVGZA3'
+              , 'DE000BAY0017'
+              , 'BE0003796134'
+              , 'SG1G55870362'
+              , 'GB0001411924'
+              , 'DE000WCH8881'
+              , 'PLLWBGD00016'
+            ]
+          , invalid: [
+                'DE000BAY0018'
+              , 'PLLWBGD00019'
+              , 'foo'
+              , '5398228707871528'
+            ]
+        });
+    });
 
     it('should validate ISBNs', function () {
         test({
