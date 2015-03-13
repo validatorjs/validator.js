570889bf1b3c963439871a0c15aa5801ef6322d7
EgoAleSum <EgoAleSum@users.noreply.github.com>
2015-03-13 14:43:41 -0400

    Escaping backticks in validator.escape()
    Without proper escaping, this could lead to XSS in IE9.
    References:
    - https://html5sec.org/#102
    - https://html5sec.org/#108
    - https://html5sec.org/#133
    
    The same rule is followed by lodash, as seen here: https://github.com/lodash/lodash/blob/master/lodash.src.js#L9917

--- test/sanitizers.js
+++ test/sanitizers.js
@@ -116,6 +116,7 @@ describe('Sanitizers', function () {
           , expect: {
                 '<script> alert("xss&fun"); </script>': '&lt;script&gt; alert(&quot;xss&amp;fun&quot;); &lt;&#x2F;script&gt;'
               , "<script> alert('xss&fun'); </script>": '&lt;script&gt; alert(&#x27;xss&amp;fun&#x27;); &lt;&#x2F;script&gt;'
+              , 'Backtick: `': 'Backtick: &#96;'
             }
         });
     });
