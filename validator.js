00567f741b893cfe8257838c66d228e7102b0995
Ben Smith <benjsmi@us.ibm.com>
2015-06-22 15:57:30 -0400

    Add validator and test for groupOfWords

--- validator.js
+++ validator.js
@@ -34,6 +34,8 @@
     'use strict';
 
     validator = { version: '3.40.1' };
+    
+    var groupOfWords = /^([0-9A-Za-z\s]+)$/;
 
     var emailUser = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e])|(\\[\x01-\x09\x0b\x0c\x0d-\x7f])))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))$/i;
 
@@ -375,6 +377,10 @@
 
     validator.isNumeric = function (str) {
         return numeric.test(str);
+    };
+    
+    validator.isGroupOfWords = function (str) {
+        return groupOfWords.test(str);
     };
 
     validator.isHexadecimal = function (str) {
