2487421d757177a29e2a61ffed236bfd4a7e5421
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2016-02-11 22:59:20 +0200

    Restore test for AMD * define is a giant hack, looks like it will handle everything you throw at it * Here is the implementation https://github.com/jrburke/requirejs/blob/a1da39714bc3eea9ab85c77e47d544e815e2699f/require.js#L2049

--- test/validators.js
+++ test/validators.js
@@ -1468,6 +1468,20 @@ describe('Validators', function () {
         });
     });
 
+    it('should define the module using an AMD-compatible loader', function () {
+        var window = {
+            validator: null
+          , define: function (module) {
+                window.validator = module();
+            }
+        };
+        window.define.amd = true;
+
+        var sandbox = vm.createContext(window);
+        vm.runInContext(validator_js, sandbox);
+        assert.equal(window.validator.trim('  foobar '), 'foobar');
+    });
+
     it('should bind validator to the window if no module loaders are available', function () {
         var window = {};
         var sandbox = vm.createContext(window);
