7832ee3a2e1b37796d763bc18e3491450ef95101
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2016-02-13 12:39:26 +0200

    Remove unnecessary prototype check in toDate

--- src/lib/toDate.js
+++ src/lib/toDate.js
@@ -1,9 +1,6 @@
 import assertString from '../util/assertString';
 
 export default function toDate(date) {
-  if (Object.prototype.toString.call(date) === '[object Date]') {
-    return date;
-  }
   assertString(date);
   date = Date.parse(date);
   return !isNaN(date) ? new Date(date) : null;
