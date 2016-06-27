1f33d75ae0c99a6ead8013c07daf200fcf907a71
suhaotian <suhaotian@users.noreply.github.com>
2016-06-27 20:51:42 +0800

    fix eslint error

--- src/lib/util/assertString.js
+++ src/lib/util/assertString.js
@@ -2,5 +2,5 @@ export default function assertString(input) {
   if (typeof input !== 'string' && input) {
     throw new TypeError('This library (validator.js) validates strings and false only');
   }
-  return (input || '')
+  return (input || '');
 }
