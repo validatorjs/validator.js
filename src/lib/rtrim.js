057d3b05c316a7c51bdeb3f8e0439fe2ed1b4295
Chris O'Hara <cohara87@gmail.com>
2016-07-21 12:58:41 +1000

    Rewrite rtrim() to avoid a regex DoS

--- src/lib/rtrim.js
+++ src/lib/rtrim.js
@@ -2,6 +2,12 @@ import assertString from './util/assertString';
 
 export default function rtrim(str, chars) {
   assertString(str);
-  const pattern = chars ? new RegExp(`[${chars}]+$`, 'g') : /\s+$/g;
-  return str.replace(pattern, '');
+  const pattern = chars ? new RegExp(`[${chars}]`) : /\s/;
+
+  let idx = str.length - 1;
+  while (idx >= 0 && pattern.test(str[idx])) {
+    idx--;
+  }
+
+  return idx < str.length ? str.substr(0, idx + 1) : str;
 }
