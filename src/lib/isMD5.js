065377d81e4de9b2c158a2367375805aba1b2fc3
gogi.strat <gogi.strat@gmail.com>
2016-07-29 04:12:37 -0400

    removed unnecessary lint suppression

--- src/lib/isMD5.js
+++ src/lib/isMD5.js
@@ -1,8 +1,6 @@
 import assertString from './util/assertString';
 
-/* eslint-disable no-control-regex */
 const md5 = /^[a-f0-9]{32}$/;
-/* eslint-enable no-control-regex */
 
 export default function isMD5(str) {
   assertString(str);
