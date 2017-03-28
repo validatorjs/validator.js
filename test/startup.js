7ab16dc9efa0721e96a7608060ac69c502425ee0
Terry Novis <terry@biits.io>
2017-03-28 13:28:04 +0100

    one more attempt to disable eslint

--- test/startup.js
+++ test/startup.js
@@ -2,14 +2,12 @@ var assert = require('assert');
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 var clearRequire = require('clear-require');
 
-let validator = '';
-
 describe('Startup', function () {
   it('should take <10ms to load', function () {
     clearRequire.all();
     const start = process.hrtime();
-    // eslint-disable-line global-require
-    validator = require('../validator');
+    // eslint-disable-next-line global-require
+    let validator = require('../validator'); // eslint-disable-line no-unused-vars
     const end = process.hrtime(start);
     const time = (end[0] * 1000) + (end[1] / 1000000);
     assert(time < 10);
