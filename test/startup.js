a17bc94b85fba908455d88752198eaeca694cade
Terry Novis <terry@biits.io>
2017-03-28 12:52:36 +0100

    Fixed linting

--- test/startup.js
+++ test/startup.js
@@ -1,13 +1,14 @@
 var assert = require('assert');
 var clearRequire = require('clear-require');
+let validator = '';
 
-describe ('Startup', function () {
-  it ('should take <10ms to load', function () {
-    clearRequire.all()
+describe('Startup', function() {
+  it('should take <10ms to load', function() {
+    clearRequire.all();
     const start = process.hrtime();
-    let validator = require('../validator');
+    validator = require('../validator');
     const end = process.hrtime(start);
     const time = (end[0] * 1000) + (end[1] / 1000000);
-    assert(time<10);
+    assert(time < 10);
   });
 });
