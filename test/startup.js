ee6bc06499fa7884447c0925a71a88e7c636b1f5
Terry Novis <terry@biits.io>
2017-03-28 13:00:52 +0100

    Adding lint overrides for test

--- test/startup.js
+++ test/startup.js
@@ -1,11 +1,14 @@
 var assert = require('assert');
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 var clearRequire = require('clear-require');
+
 let validator = '';
 
-describe('Startup', function() {
-  it('should take <10ms to load', function() {
+describe('Startup', function () {
+  it('should take <10ms to load', function () {
     clearRequire.all();
     const start = process.hrtime();
+    // eslint-disable-line global-require
     validator = require('../validator');
     const end = process.hrtime(start);
     const time = (end[0] * 1000) + (end[1] / 1000000);
