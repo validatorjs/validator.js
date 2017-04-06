520bf19817449e9704f24ef62fa4ac54514e3c13
Marcin K <schizek.marcin@gmail.com>
2017-04-06 22:40:05 +0100

    Update assertString.js

--- src/lib/util/assertString.js
+++ src/lib/util/assertString.js
@@ -1,6 +1,6 @@
 export default function assertString(input) {
   const isString = (typeof input === 'string' || input instanceof String);
-  
+
   if (!isString) {
     throw new TypeError('This library (validator.js) validates strings only');
   }
