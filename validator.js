e03c720e53715c34135a39fac38ac22bd318a03f
Kanitkorn S <k.sujautra@gmail.com>
2017-08-15 10:31:44 +0700

    Build the change for unescape function

--- validator.js
+++ validator.js
@@ -1084,7 +1084,7 @@ function escape(str) {
 
 function unescape(str) {
   assertString(str);
-  return str.replace(/&amp;/g, '&').replace(/&quot;/g, '"').replace(/&#x27;/g, "'").replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&#x2F;/g, '/').replace(/&#96;/g, '`');
+  return str.replace(/&amp;/g, '&').replace(/&quot;/g, '"').replace(/&#x27;/g, "'").replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&#x2F;/g, '/').replace(/&#x5C;/g, '\\').replace(/&#96;/g, '`');
 }
 
 function blacklist(str, chars) {
