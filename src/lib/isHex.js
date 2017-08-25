b6808d828494152fc1a1926b73c94c404c4c9087
Damian <kontakt@dszymczuk.pl>
2017-08-25 20:05:19 +0200

    isHex - lint fix

--- src/lib/isHex.js
+++ src/lib/isHex.js
@@ -3,35 +3,36 @@ import assertString from './util/assertString';
 import merge from './util/merge';
 
 const default_ishex_options = {
-  allow_transaprent: true,
+  allow_transparent: true,
 };
-
-/* eslint-disable max-len */
-/* eslint-disable no-control-regex */
-/* eslint-enable max-len */
-/* eslint-enable no-control-regex */
 
 export default function isHex(val, options) {
   assertString(val);
-	options = merge(options, default_ishex_options);
+  options = merge(options, default_ishex_options);
 
-  if(default_ishex_options.allow_transaprent && val === 'transparent'){
-        return true;
+  if (options.allow_transparent && val === 'transparent') {
+    return true;
+  }
+
+  let startWithHex = val[0] === '#';
+
+  if (!startWithHex) {
+    return false;
+  }
+
+  let isCorrectLength = val.length === 4 || val.length === 7;
+
+  if (isCorrectLength) {
+    let regex = /[0-9a-f]/i;
+    let valueSliced = val.slice(1).split('');
+    let isValid = true;
+    valueSliced.forEach((i) => {
+      if (i.match(regex) === null) {
+        isValid = false;
       }
+    });
+    return isValid;
+  }
 
-      var startWithHex = val[0] === "#";
-      var isCorrectLength = val.length === 4 || val.length === 7;
-
-      if(isCorrectLength){
-        var regex = /[0-9a-f]/i;
-        var valueSliced = val.slice(1).split('');
-        var isValid = true;
-        valueSliced.forEach(function(i){
-          if(i.match(regex) === null)
-            isValid = false;
-        });
-        return isValid;
-      }
-
-      return false;
+  return false;
 }
