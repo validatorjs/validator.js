4b3f548055352a8d79e527f9ec213029a9cd720e
Damian <kontakt@dszymczuk.pl>
2017-08-25 20:10:04 +0200

    isHsla - lint fix

--- src/lib/isHsla.js
+++ src/lib/isHsla.js
@@ -3,79 +3,64 @@ import assertString from './util/assertString';
 import merge from './util/merge';
 
 const default_ishsla_options = {
-  allow_transaprent: true,
+  allow_transparent: true,
 };
 
-var filterFloat = function(value) {
-    if (/^(\-|\+)?([0-9]+(\.[0-9]+)?|Infinity)$/
-      .test(value))
-      return Number(value);
+let filterFloat = function (value) {
+  if (/^(\-|\+)?([0-9]+(\.[0-9]+)?|Infinity)$/
+    .test(value)) {
+    return Number(value);
+  }
   return NaN;
-}
-
-var isBetween0and1 = function(value){
-  return value > 0 && value < 1;
-}
-
-/* eslint-disable max-len */
-/* eslint-disable no-control-regex */
-/* eslint-enable max-len */
-/* eslint-enable no-control-regex */
+};
 
 export default function isHsla(val, options) {
   assertString(val);
-	options = merge(options, default_ishsla_options);
+  options = merge(options, default_ishsla_options);
 
-      if(default_ishsla_options.allow_transaprent === 'true' && val === 'transparent'){
-        return true;
-      };
+  if (options.allow_transparent === 'true' && val === 'transparent') {
+    return true;
+  }
 
-      var removedSpace = val.replace(/ /g, '');
-      var regex = /\([0-9]{1,3},[0-9]{1,3}%,[0-9]{1,3}%,[0,1]{1}.?[0-9]*\)/i;
+  let removedSpace = val.replace(/ /g, '');
+  let regex = /\([0-9]{1,3},[0-9]{1,3}%,[0-9]{1,3}%,[0,1]{1}.?[0-9]*\)/i;
 
-      if(removedSpace.match(regex)){
-          var removeBrackets = removedSpace.replace(/\(/g, '').replace(/\)/g, '');
-          var valueSliced = removeBrackets.split(',');
-          var isValid = true;
+  if (removedSpace.match(regex)) {
+    let removeBrackets = removedSpace.replace(/\(/g, '').replace(/\)/g, '');
+    let valueSliced = removeBrackets.split(',');
+    let isValid = true;
 
-          valueSliced.forEach(function(i,index){
-            var value = filterFloat(i);
+    valueSliced.forEach((i, index) => {
+      let value = filterFloat(i);
 
-            if(Number.isInteger(value)){
-                if(index === 0){
-                  var isInRange = 0 <= value && value <= 360;
-                  if(!isInRange){
-                    isValid = false;
-                  }
-                } else {
-
-                  console.log(value);
-
-                  var isInRange = 0 <= value && value <= 100;
-                  if(!isInRange){
-                    isValid = false;
-                  }
-                }
-            } else {
-              if(isNaN(value)){
-                // percent value
-                value = parseInt(i);
-                var isInRange = 0 <= value && value <= 100;
-                if(!isInRange){
-                  isValid = false;
-                }
-              } else {
-                var isInRange = 0 <= value && value <= 1;
-                if(!isInRange){
-                  isValid = false;
-                }
-              }
-            }
-        });
-        return isValid;
+      if (Number.isInteger(value)) {
+        if (index === 0) {
+          const isInRange = value >= 0 && value <= 360;
+          if (!isInRange) {
+            isValid = false;
+          }
+        } else {
+          const isInRange = value >= 0 && value <= 100;
+          if (!isInRange) {
+            isValid = false;
+          }
+        }
+      } else if (isNaN(value)) {
+        // percent value
+        value = parseInt(i, 10);
+        const isInRange = value >= 0 && value <= 100;
+        if (!isInRange) {
+          isValid = false;
+        }
       } else {
-        console.log("Bad regex");
+        const isInRange = value >= 0 && value <= 1;
+        if (!isInRange) {
+          isValid = false;
+        }
       }
+    });
+    return isValid;
+  }
 
-      return false;
+  return false;
 }
