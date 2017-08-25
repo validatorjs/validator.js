6a30160759bbf6fbddbba5cafdd0f66c8d870164
Damian <kontakt@dszymczuk.pl>
2017-08-25 20:03:58 +0200

    isRgb - lint fix

--- src/lib/isRgb.js
+++ src/lib/isRgb.js
@@ -3,37 +3,33 @@ import assertString from './util/assertString';
 import merge from './util/merge';
 
 const default_isrgb_options = {
-  allow_transaprent: true,
+  allow_transparent: true,
 };
-
-/* eslint-disable max-len */
-/* eslint-disable no-control-regex */
-/* eslint-enable max-len */
-/* eslint-enable no-control-regex */
 
 export default function isRgb(val, options) {
   assertString(val);
-	options = merge(options, default_isrgb_options);
+  options = merge(options, default_isrgb_options);
 
-if(default_isrgb_options.allow_transaprent === 'true' && val === 'transparent'){
-        return true;
-      };
+  if (options.allow_transparent === 'true' && val === 'transparent') {
+    return true;
+  }
 
-      var removedSpace = val.replace(/ /g, '');
-      var regex = /\([0-9]{1,3},[0-9]{1,3},[0-9]{1,3}\)/i;
+  let removedSpace = val.replace(/ /g, '');
+  let regex = /\([0-9]{1,3},[0-9]{1,3},[0-9]{1,3}\)/i;
 
-      if(removedSpace.match(regex)){
-          var removeBrackets = removedSpace.replace(/\(/g, '').replace(/\)/g, '');
-          var valueSliced = removeBrackets.split(',');
-          var isValid = true;
+  if (removedSpace.match(regex)) {
+    let removeBrackets = removedSpace.replace(/\(/g, '').replace(/\)/g, '');
+    let valueSliced = removeBrackets.split(',');
+    let isValid = true;
 
-          valueSliced.forEach(function(i){
-            var parsedInt = parseInt(i);
-            if((Number.isInteger(parsedInt) && 0 <= parsedInt && parsedInt <= 255) === false)
-              isValid = false;
-        });
-        return isValid;
+    valueSliced.forEach((i) => {
+      let parsedInt = parseInt(i, 10);
+      if ((Number.isInteger(parsedInt) && parsedInt >= 0 && parsedInt <= 255) === false) {
+        isValid = false;
       }
+    });
+    return isValid;
+  }
 
-      return false;
+  return false;
 }
