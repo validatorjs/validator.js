a4d0ac65adf31538f6b5edf69cbe936f30b50611
PoqJD <jd@poqstudio.com>
2017-10-02 11:45:45 +0100

    Add ISO-3166-1 alpha-2 validation

--- test/validators.js
+++ test/validators.js
@@ -4716,6 +4716,37 @@ describe('Validators', function () {
     });
   });
 
+  it('should validate ISO 3166-1 alpha 2 country codes', function () {
+    // from https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
+    test({
+      validator: 'isISO31661Alpha2',
+      valid: [
+        'FR',
+        'fR',
+        'GB',
+        'PT',
+        'CM',
+        'JP',
+        'PM',
+        'ZW',
+        'MM',
+        'cc',
+        'GG',
+      ],
+      invalid: [
+        '',
+        'FRA',
+        'AA',
+        'PI',
+        'RP',
+        'WV',
+        'WL',
+        'UK',
+        'ZZ',
+      ],
+    });
+  });
+
   it('should validate whitelisted characters', function () {
     test({
       validator: 'isWhitelisted',
