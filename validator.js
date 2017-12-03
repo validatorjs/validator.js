0f18a2ee227aa032a0fcef1524f615369fe50df6
Tomas <tomastrg22@gmail.com>
2017-12-03 12:17:49 +0100

    remove console.log

--- validator.js
+++ validator.js
@@ -1116,6 +1116,23 @@ function isCurrency(str, options) {
   return currencyRegex(options).test(str);
 }
 
+var spanishDNI = /(\d{8})(\s|-)?([a-zA-Z]{1})$/;
+
+function isSpanishDNI(str) {
+  assertString(str);
+  var isValidFormat = spanishDNI.test(str);
+  if (!isValidFormat) {
+    return false;
+  }
+  var letter = str[str.length - 1];
+  var encoder = 'TRWAGMYFPDXBNJZSQVHLCKE';
+  var sum = str.replace(/[ a-zA-Z_-]+/, '').split('').reduce(function (acc, digit) {
+    return acc + Number(digit);
+  });
+
+  return letter.toUpperCase() === encoder[sum % 23];
+}
+
 /* eslint-disable max-len */
 // from http://goo.gl/0ejHHW
 var iso8601 = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
