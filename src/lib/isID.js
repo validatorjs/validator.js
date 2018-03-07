b1d6945c53165e3dab3dbfe8d2cfd73103b12d1f
林泉 <linquan@hexindai.com>
2018-03-07 17:24:31 +0800

    fixed some bug

--- src/lib/isID.js
+++ src/lib/isID.js
@@ -18,9 +18,19 @@ const IDS = {
     return letter.toUpperCase() === encoder[sum % 23];
   },
   'ko-KR': function (str) {
-    if (!/^\d{6}\-?(1|2)\d{6}$/.test(str)) {
+    let birth = str.match(/^(\d{2})(\d{2})(\d{2})\-?([1-4])\d{6}$/);
+    if (!birth) {
       return false;
     }
+
+    let bd_txt = `${(birth[4] > 2 ? '20' : '19') + birth[1]}/${parseInt(birth[2], 10)}/${parseInt(birth[3], 10)}`;
+    let bd = new Date(bd_txt);
+    let new_bd_txt = `${bd.getFullYear()}/${bd.getMonth() + 1}/${bd.getDate()}`;
+    let bd_check = (bd_txt === new_bd_txt);
+    if (!bd_check) {
+      return false;
+    }
+
     str = str.replace(/\-/g, '');
     let len = str.length;
     if (len !== 13) {
@@ -43,30 +53,37 @@ const IDS = {
     if (len !== 18) {
       return false;
     }
-    if (!/(^\d{15}$)|(^\d{17}([0-9]|X)$)/.test(str)) {
+
+    const city_code = [11, 12, 13, 14, 15, 21, 22, 23, 31, 32, 33, 34, 35, 36, 37, 41, 42, 43, 44, 45, 46, 50, 51, 52, 53, 54, 61, 62, 63, 64, 65, 71, 81, 82, 91];
+    if (city_code.indexOf[parseInt(str.substr(0, 2), 10)] === -1) {
       return false;
     }
-    const aCity = [11, 12, 13, 14, 15, 21, 22, 23, 31, 32, 33, 34, 35, 36, 37, 41, 42, 43, 44, 45, 46, 50, 51, 52, 53, 54, 61, 62, 63, 64, 65, 71, 81, 82, 91];
-    if (aCity.indexOf[parseInt(str.substr(0, 2), 10)] === -1) {
+
+    let birth = str.match(/^(\d{6})(\d{4})(\d{2})(\d{2})(\d{3})([0-9]|X)$/);
+    if (!birth) {
       return false;
     }
-    const re = /^(\d{6})(\d{4})(\d{2})(\d{2})(\d{3})([0-9]|X)$/;
-    let arrSplit = str.match(re);
-    let dtmBirth = new Date(`${arrSplit[2]}'/'${arrSplit[3]}'/'${arrSplit[4]}`);
-    let bGoodDay = dtmBirth.getFullYear() === Number(arrSplit[2]) && dtmBirth.getMonth() + 1 === Number(arrSplit[3]) && dtmBirth.getDate() === Number(arrSplit[4]);
-    if (!bGoodDay) {
+
+    let bd_txt = `${birth[2]}/${parseInt(birth[3], 10)}/${parseInt(birth[4], 10)}`;
+    let bd = new Date(bd_txt);
+    let new_bd_txt = `${bd.getFullYear()}/${bd.getMonth() + 1}/${bd.getDate()}`;
+    console.log(bd_txt, new_bd_txt);
+    let bd_check = (bd_txt === new_bd_txt);
+    if (!bd_check) {
       return false;
     }
-    let nTemp = 0;
-    const arrInt = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
-    const arrCh = ['1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'];
+
+    let result = 0;
+    const ckint = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
+    const ck = ['1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'];
     for (let i = 0; i < 17; i++) {
-      nTemp += str.substr(i, 1) * arrInt[i];
+      result += str.substr(i, 1) * ckint[i];
     }
-    let valstr = arrCh[nTemp % 11];
-    if (valstr === str.substr(17, 1)) {
+    result = ck[result % 11];
+    if (result === str.substr(17, 1)) {
       return true;
     }
+
     return false;
   },
 };
