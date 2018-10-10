23de32e0f216815ffc5c1915e84109f602c4e4d0
Alessandro Segala <ItalyPaleAle@users.noreply.github.com>
2018-10-09 17:48:28 -0700

    Updated isJWT so it's conforming to specs

--- src/lib/isJWT.js
+++ src/lib/isJWT.js
@@ -1,6 +1,6 @@
 import assertString from './util/assertString';
 
-const jwt = /^[a-zA-Z0-9\-_]+\.[a-zA-Z0-9\-_]+\.[a-zA-Z0-9\-_]+$/;
+const jwt = /^([A-Za-z0-9\-_~+\/]+[=]{0,2})\.([A-Za-z0-9\-_~+\/]+[=]{0,2})(?:\.([A-Za-z0-9\-_~+\/]+[=]{0,2}))?$/;
 
 export default function isJWT(str) {
   assertString(str);
