dea9f3e9820126733abd13ca1fb39df580bd5fbc
Maxim Mazurok <maxim@mazurok.com>
2018-11-30 14:32:57 -0800

    Added UA postal code validation, updated tests, browser build, and readme

--- test/validators.js
+++ test/validators.js
@@ -6090,6 +6090,14 @@ describe('Validators', () => {
           'AD700',
         ],
       },
+      {
+        locale: 'UA',
+        valid: [
+          '65000',
+          '65080',
+          '01000',
+        ],
+      },
     ];
 
     let allValid = [];
