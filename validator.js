58c95e4091414b592edb526300217516023223d4
idrisakmal <idrisakmalhaslin@gmail.com>
2018-12-14 22:06:32 +0800

    Added test for MY numbers, should be good now

--- validator.js
+++ validator.js
@@ -1,6 +1,6 @@
 /*!
  * Copyright (c) 2018 Chris O'Hara <cohara87@gmail.com>
- *
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -8,10 +8,10 @@
  * distribute, sublicense, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
- *
+ * 
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -1822,7 +1822,7 @@ function rtrim(str, chars) {
   var idx = str.length - 1;
 
   for (; idx >= 0 && pattern.test(str[idx]); idx--) {
-
+    
   }
 
   return idx < str.length ? str.substr(0, idx + 1) : str;
