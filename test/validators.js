6d4f3fedc7add04a817d560f957049fe0fc98170
razananisathar <frazana.oa@gmail.com>
2019-05-04 20:51:38 +0530

    Test isBase32 validator

--- test/validators.js
+++ test/validators.js
@@ -3261,6 +3261,32 @@ describe('Validators', () => {
     });
   });
 
+  it('should validate base32 strings', () => {
+    test({
+      validator: 'isBase32',
+      valid: [
+        'ZG======',
+        'JBSQ====',
+        'JBSWY===',
+        'JBSWY3A=',
+        'JBSWY3DP',
+        'JBSWY3DPEA======',
+        'K5SWYY3PNVSSA5DPEBXG6ZA=',
+        'K5SWYY3PNVSSA5DPEBXG6===',
+      ],
+      invalid: [
+        '12345',
+        '',
+        'JBSWY3DPtesting123',
+        'ZG=====',
+        'Z======',
+        'Zm=8JBSWY3DP',
+        '=m9vYg==',
+        'Zm9vYm/y====',
+      ],
+    });
+  });
+
   it('should validate base64 strings', () => {
     test({
       validator: 'isBase64',
