dc447b60dd736b02a50e1202725046e565b01889
me-x-mi <ezrqnkemboi@gmail.com>
2019-07-20 12:25:12 +0300

    add use of hyphen or spaces in mac address - Authoured_by: Ezrqn Kemboi <ezrqnkemboi@gmail.com> - Closes #1061

--- src/lib/isMACAddress.js
+++ src/lib/isMACAddress.js
@@ -2,11 +2,13 @@ import assertString from './util/assertString';
 
 const macAddress = /^([0-9a-fA-F][0-9a-fA-F]:){5}([0-9a-fA-F][0-9a-fA-F])$/;
 const macAddressNoColons = /^([0-9a-fA-F]){12}$/;
+const macAddressWithHyphen = /^([0-9a-fA-F][0-9a-fA-F]-){5}([0-9a-fA-F][0-9a-fA-F])$/;
+const macAddressWithSpaces = /^([0-9a-fA-F][0-9a-fA-F]\s){5}([0-9a-fA-F][0-9a-fA-F])$/;
 
 export default function isMACAddress(str, options) {
   assertString(str);
   if (options && options.no_colons) {
     return macAddressNoColons.test(str);
   }
-  return macAddress.test(str);
+  return macAddress.test(str) || macAddressWithHyphen.test(str) || macAddressWithSpaces.test(str);
 }
