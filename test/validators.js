ed6ebb203b5ecadf77dfb716d611b14932184696
Christopher AYALA <chris.ayala@ieee.org>
2019-10-12 22:14:12 +0900

    Added 0h-prefix as an acceptable hexadecimal format.

--- test/validators.js
+++ test/validators.js
@@ -2497,6 +2497,11 @@ describe('Validators', () => {
         'ff0044',
         '0xff0044',
         '0XfF0044',
+        '0x0123456789abcDEF',
+        '0X0123456789abcDEF',
+        '0hfedCBA9876543210',
+        '0HfedCBA9876543210',
+        '0123456789abcDEF',
       ],
       invalid: [
         'abcdefg',
@@ -2504,6 +2509,9 @@ describe('Validators', () => {
         '..',
         '0xa2h',
         '0xa20x',
+        '0x0123456789abcDEFq',
+        '0hfedCBA9876543210q',
+        '01234q56789abcDEF',
       ],
     });
   });
