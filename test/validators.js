1bf00948fd60367db1645261ca5ce7d6aef097b6
Chris <chrislayala@gmail.com>
2019-10-13 19:38:27 +0900

    fix: add validation for '0x'-prefixed hex strings (#1147)
    * Added validation for '0x'-prefixed hex strings.
    
    * Added 0h-prefix as an acceptable hexadecimal format.

--- test/validators.js
+++ test/validators.js
@@ -2495,11 +2495,23 @@ describe('Validators', () => {
       valid: [
         'deadBEEF',
         'ff0044',
+        '0xff0044',
+        '0XfF0044',
+        '0x0123456789abcDEF',
+        '0X0123456789abcDEF',
+        '0hfedCBA9876543210',
+        '0HfedCBA9876543210',
+        '0123456789abcDEF',
       ],
       invalid: [
         'abcdefg',
         '',
         '..',
+        '0xa2h',
+        '0xa20x',
+        '0x0123456789abcDEFq',
+        '0hfedCBA9876543210q',
+        '01234q56789abcDEF',
       ],
     });
   });
