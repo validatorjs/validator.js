6af17cab63261759fa4d68fce2457a957742a980
Rohan <rohankulshreshtha.bkn@gmail.com>
2019-10-16 20:15:55 +0530

    fixed the linting issue

--- src/lib/isLatLong.js
+++ src/lib/isLatLong.js
@@ -6,7 +6,7 @@ const long = /^\s?[+-]?(180(\.0+)?|1[0-7]\d(\.\d+)?|\d{1,2}(\.\d+)?)\)?$/;
 const dms_lat = /^(([1-8]?\d)\D+([1-5]?\d|60)\D+([1-5]?\d|60)(\.\d+)?|90\D+0\D+0)\D+[NSns]?$/;
 const dms_long = /^\s*([1-7]?\d{1,2}\D+([1-5]?\d|60)\D+([1-5]?\d|60)(\.\d+)?|180\D+0\D+0)\D+[EWew]?$/;
 
-export default function _default(str,options) {
+export default function isLatLong(str, options) {
   assertString(str);
   if (!str.includes(',')) return false;
   const pair = str.split(',');
