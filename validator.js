8b929d36f00f6d13b48c995f29943aecbe921aae
Chris O'Hara <cohara87@gmail.com>
2019-11-21 15:22:10 +1000

    chore: sync compiled/min versions

--- validator.js
+++ validator.js
@@ -49,6 +49,10 @@ function _arrayWithHoles(arr) {
 }
 
 function _iterableToArrayLimit(arr, i) {
+  if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) {
+    return;
+  }
+
   var _arr = [];
   var _n = true;
   var _d = false;
