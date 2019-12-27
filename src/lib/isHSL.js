0a71904d37820fbb496831fe76ef10576f4c0050
Christopher Ayala <chris.ayala@ieee.org>
2019-12-27 22:32:47 +0900

    WIP on validating degree values beyond [0,360]

--- src/lib/isHSL.js
+++ src/lib/isHSL.js
@@ -1,6 +1,6 @@
 import assertString from './util/assertString';
 
-const hsl = /^(hsl)\(\s*0*([0-2]?[0-9]?[0-9]|3[0-5][0-9]|360)\s*,\s*0*(0?[0-9]?[0-9]|100)%\s*,\s*0*(0?[0-9]?[0-9]|100)%\s*\)$/i;
+const hsl = /^(hsl)\(\s*(\+|\-)?0*[0-9]+\s*,\s*0*(0?[0-9]?[0-9]|100)%\s*,\s*0*(0?[0-9]?[0-9]|100)%\s*\)$/i;
 
 export default function isHSL(str) {
   assertString(str);
