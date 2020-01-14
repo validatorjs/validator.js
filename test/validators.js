581451090ebc3e78b7439c06c9c3771032ce0540
Adam Conrad <adamconrad@MBP-19-C02XD63GJHD2.local>
2020-01-14 13:25:25 -0500

    Support CSS Colors Level 4 spec

--- test/validators.js
+++ test/validators.js
@@ -2588,14 +2588,16 @@ describe('Validators', () => {
     test({
       validator: 'isHexColor',
       valid: [
+        '#ff0000ff',
         '#ff0034',
         '#CCCCCC',
+        '0f38',
         'fff',
         '#f00',
       ],
       invalid: [
         '#ff',
-        'fff0',
+        'fff0a',
         '#ff12FG',
       ],
     });
