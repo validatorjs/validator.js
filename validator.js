a6cebd8582cfcb049b3d4f2a30ce00b2e950c5a5
Abdullah Danyal Saeed <abdullahsaeed92@gmail.com>
2020-04-02 00:14:59 +0300

    Revert changes in validator

--- validator.js
+++ validator.js
@@ -27,6 +27,8 @@
 }(this, (function () { 'use strict';
 
 function _typeof(obj) {
+  "@babel/helpers - typeof";
+
   if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") {
     _typeof = function (obj) {
       return typeof obj;
@@ -41,7 +43,7 @@ function _typeof(obj) {
 }
 
 function _slicedToArray(arr, i) {
-  return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest();
+  return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest();
 }
 
 function _arrayWithHoles(arr) {
@@ -49,10 +51,7 @@ function _arrayWithHoles(arr) {
 }
 
 function _iterableToArrayLimit(arr, i) {
-  if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) {
-    return;
-  }
-
+  if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return;
   var _arr = [];
   var _n = true;
   var _d = false;
@@ -78,34 +77,48 @@ function _iterableToArrayLimit(arr, i) {
   return _arr;
 }
 
-function _nonIterableRest() {
-  throw new TypeError("Invalid attempt to destructure non-iterable instance");
+function _unsupportedIterableToArray(o, minLen) {
+  if (!o) return;
+  if (typeof o === "string") return _arrayLikeToArray(o, minLen);
+  var n = Object.prototype.toString.call(o).slice(8, -1);
+  if (n === "Object" && o.constructor) n = o.constructor.name;
+  if (n === "Map" || n === "Set") return Array.from(n);
+  if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);
 }
 
-function assertString(input) {
-  var isString = typeof input === 'string' || input instanceof String;
+function _arrayLikeToArray(arr, len) {
+  if (len == null || len > arr.length) len = arr.length;
 
-function _getTypeName(input) {
+  for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
+
+  return arr2;
+}
+
+function _nonIterableRest() {
+  throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+}
+
+function getType(input) {
+  var invalidType;
+
   if (input === null) {
-    return 'null';
-  }
-
-  var invalidType = _typeof(input);
-
-  if (invalidType === 'object' && input.constructor && input.constructor.hasOwnProperty('name')) {
-    invalidType = input.constructor.name;
+    invalidType = 'null';
   } else {
-    invalidType = "a ".concat(invalidType);
-  }
+    invalidType = _typeof(input);
 
-  return invalidType;
+    if (invalidType === 'object' && input.constructor && input.constructor.hasOwnProperty('name')) {
+      invalidType = input.constructor.name;
+    } else {
+      invalidType = "a ".concat(invalidType);
+    }
+  }
 }
 
 function assertString(input) {
   var isString = typeof input === 'string' || input instanceof String;
 
   if (!isString) {
-    throw new TypeError("Expected string but received ".concat(_getTypeName(input), "."));
+    throw new TypeError("Expected string but received ".concat(getType(input), "."));
   }
 }
 
