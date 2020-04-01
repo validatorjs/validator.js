3c593f807ebc461a3874ff06d9716eadaefbc69f
Abdullah Danyal Saeed <abdullahsaeed92@gmail.com>
2020-04-02 00:13:47 +0300

    Refactor assert string

--- src/lib/util/assertString.js
+++ src/lib/util/assertString.js
@@ -1,18 +1,22 @@
+function getType(input) {
+  let invalidType;
+
+  if (input === null) {
+    invalidType = 'null';
+  } else {
+    invalidType = typeof input;
+    if (invalidType === 'object' && input.constructor && input.constructor.hasOwnProperty('name')) {
+      invalidType = input.constructor.name;
+    } else {
+      invalidType = `a ${invalidType}`;
+    }
+  }
+}
+
 export default function assertString(input) {
   const isString = (typeof input === 'string' || input instanceof String);
 
   if (!isString) {
-    let invalidType;
-    if (input === null) {
-      invalidType = 'null';
-    } else {
-      invalidType = typeof input;
-      if (invalidType === 'object' && input.constructor && input.constructor.hasOwnProperty('name')) {
-        invalidType = input.constructor.name;
-      } else {
-        invalidType = `a ${invalidType}`;
-      }
-    }
-    throw new TypeError(`Expected string but received ${invalidType}.`);
+    throw new TypeError(`Expected string but received ${getType(input)}.`);
   }
 }
