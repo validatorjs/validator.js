2934eb99ff771e04323b3f9581a6748771984b86
Josh Rincon <joshua.rincon@rizehq.com>
2020-05-05 12:02:54 -0700

    remove all files

--- src/lib/isLatLong.js
+++ src/lib/isLatLong.js
@@ -3,7 +3,7 @@ import assertString from './util/assertString';
 const lat = /^\(?[+-]?(90(\.0+)?|[1-8]?\d(\.\d+)?)$/;
 const long = /^\s?[+-]?(180(\.0+)?|1[0-7]\d(\.\d+)?|\d{1,2}(\.\d+)?)\)?$/;
 
-export default function (str) {
+export default function isLatLong(str) {
   assertString(str);
   if (!str.includes(',')) return false;
   const pair = str.split(',');
