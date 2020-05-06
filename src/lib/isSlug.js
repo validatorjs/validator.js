723f60503783a762dafe604826639d7bb3727041
Vusal Ismayilov <vusali@crbn.az>
2020-05-06 15:04:18 +0400

    Modify isSlug func

--- src/lib/isSlug.js
+++ src/lib/isSlug.js
@@ -4,5 +4,8 @@ let charsetRegex = /^[^-_](?!.*?[-_]{2,})([a-z0-9\\-]{1,}).*[^-_]$/;
 
 export default function isSlug(str) {
   assertString(str);
+  if (str.includes(' ')) {
+    return false;
+  }
   return (charsetRegex.test(str));
 }
