e807fb914552585fea426f17372e93b0fc5037fe
Paras Gupta <parasg1999@gmail.com>
2020-05-28 15:38:53 +0530

    feat(isIdentityCard): add norway locale (#1324)
    * Fixes #1303
    
    * Added postal code for nepal
    
    * Added NP locale to postal code
    
    * Added country code to passport READNE
    
    * Added norway identity card

--- src/lib/isIdentityCard.js
+++ src/lib/isIdentityCard.js
@@ -61,7 +61,6 @@ const validators = {
 
     // sanitize user input
     const sanitized = str.trim();
-    console.log(sanitized);
 
     // validate the data structure
     if (!DNI.test(sanitized)) {
@@ -75,6 +74,26 @@ const validators = {
     });
 
     return c === 0;
+  },
+  NO: (str) => {
+    const sanitized = str.trim();
+    if (isNaN(Number(sanitized))) return false;
+    if (sanitized.length !== 11) return false;
+    if (sanitized === '00000000000') return false;
+
+    // https://no.wikipedia.org/wiki/F%C3%B8dselsnummer
+    const f = sanitized.split('').map(Number);
+    let k1 = (11 - (((3 * f[0]) + (7 * f[1]) + (6 * f[2])
+      + (1 * f[3]) + (8 * f[4]) + (9 * f[5]) + (4 * f[6])
+      + (5 * f[7]) + (2 * f[8])) % 11)) % 11;
+    let k2 = (11 - (((5 * f[0]) + (4 * f[1]) + (3 * f[2])
+      + (2 * f[3]) + (7 * f[4]) + (6 * f[5]) + (5 * f[6])
+      + (4 * f[7]) + (3 * f[8]) + (2 * k1)) % 11)) % 11;
+    if (k1 === 11) {
+      k1 = 0;
+    }
+    if (k1 !== f[9] || k2 !== f[10]) return false;
+    return true;
   },
   'he-IL': (str) => {
     const DNI = /^\d{9}$/;
