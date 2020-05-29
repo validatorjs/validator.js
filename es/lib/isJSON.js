bdac87d832763a0dd62114722466871a686e9c20
LeviathanCalumet <matthieu.vallance@premista.fr>
2020-05-29 17:16:26 +0200

    Resting unrelated generated files

--- es/lib/isJSON.js
+++ es/lib/isJSON.js
@@ -1,23 +1,12 @@
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 import assertString from './util/assertString';
-import merge from './util/merge';
-var default_json_options = {
-  allow_primitives: false
-};
-export default function isJSON(str, options) {
+export default function isJSON(str) {
   assertString(str);
 
   try {
-    options = merge(options, default_json_options);
-    var primitives = [];
-
-    if (options.allow_primitives) {
-      primitives = [null, false, true];
-    }
-
     var obj = JSON.parse(str);
-    return primitives.includes(obj) || !!obj && _typeof(obj) === 'object';
+    return !!obj && _typeof(obj) === 'object';
   } catch (e) {
     /* ignore */
   }
