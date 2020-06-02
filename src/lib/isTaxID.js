5e13b266b8ac83bfe855b069bbb42f24106cb6a7
Rubin <roobin.bhandari@gmail.com>
2020-06-02 20:49:58 +0545

    locale added for taxid

--- src/lib/isTaxID.js
+++ src/lib/isTaxID.js
@@ -14,48 +14,55 @@ import assertString from './util/assertString';
 
 
 /**
- * Campus prefixes.
+ * Campus prefixes according to locales
  */
 
 const campusPrefix = {
-  andover: ['10', '12'],
-  atlanta: ['60', '67'],
-  austin: ['50', '53'],
-  brookhaven: ['01', '02', '03', '04', '05', '06', '11', '13', '14', '16', '21', '22', '23', '25', '34', '51', '52', '54', '55', '56', '57', '58', '59', '65'],
-  cincinnati: ['30', '32', '35', '36', '37', '38', '61'],
-  fresno: ['15', '24'],
-  internet: ['20', '26', '27', '45', '46', '47'],
-  kansas: ['40', '44'],
-  memphis: ['94', '95'],
-  ogden: ['80', '90'],
-  philadelphia: ['33', '39', '41', '42', '43', '46', '48', '62', '63', '64', '66', '68', '71', '72', '73', '74', '75', '76', '77', '81', '82', '83', '84', '85', '86', '87', '88', '91', '92', '93', '98', '99'],
-  sba: ['31'],
+  'en-US': {
+    andover: ['10', '12'],
+    atlanta: ['60', '67'],
+    austin: ['50', '53'],
+    brookhaven: ['01', '02', '03', '04', '05', '06', '11', '13', '14', '16', '21', '22', '23', '25', '34', '51', '52', '54', '55', '56', '57', '58', '59', '65'],
+    cincinnati: ['30', '32', '35', '36', '37', '38', '61'],
+    fresno: ['15', '24'],
+    internet: ['20', '26', '27', '45', '46', '47'],
+    kansas: ['40', '44'],
+    memphis: ['94', '95'],
+    ogden: ['80', '90'],
+    philadelphia: ['33', '39', '41', '42', '43', '46', '48', '62', '63', '64', '66', '68', '71', '72', '73', '74', '75', '76', '77', '81', '82', '83', '84', '85', '86', '87', '88', '91', '92', '93', '98', '99'],
+    sba: ['31'],
+  },
 };
 
 
-/**
- * Cache all available prefixes.
- */
+function getPrefixes(locale) {
+  const prefixes = [];
 
-const prefixes = [];
-
-for (const location in campusPrefix) {
-  if (campusPrefix.hasOwnProperty(location)) {
-    prefixes.push(...campusPrefix[location]);
+  for (const location in campusPrefix[locale]) {
+    if (campusPrefix[locale].hasOwnProperty(location)) {
+      prefixes.push(...campusPrefix[locale][location]);
+    }
   }
+
+  prefixes.sort();
+
+  return prefixes;
 }
 
-prefixes.sort();
+// tax id regex formats for various loacles
+
+const taxIdFormat = {
+
+  'en-US': /^\d{2}[- ]{0,1}\d{7}$/,
+
+};
 
 
-const taxIdFormat = /^\d{2}[- ]{0,1}\d{7}$/;
-
-
-export default function isTaxID(str) {
+export default function isTaxID(str, locale = 'en-US') {
   assertString(str);
-  if (!taxIdFormat.test(str)) {
+  if (!taxIdFormat[locale].test(str)) {
     return false;
   }
-  return prefixes.indexOf(str.substr(0, 2)) !== -1;
+  return getPrefixes(locale).indexOf(str.substr(0, 2)) !== -1;
 }
 
