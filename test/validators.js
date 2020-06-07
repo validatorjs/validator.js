f815e4bfe46e24fc14c19ebd42e106d6ff124b7d
Tim Beck <socaltdb@gmail.com>
2020-06-07 13:16:22 -0700

    Add tests

--- test/validators.js
+++ test/validators.js
@@ -8475,6 +8475,28 @@ describe('Validators', () => {
     });
   });
 
+  it('should validate strong passwords', () => {
+    test({
+      validator: 'isStrongPassword',
+      valid: [
+        '%2%k{7BsL"M%Kd6e',
+        'EXAMPLE of very long_password!',
+        'mxH_+2vs&54_+H3P',
+        '+&DxJ=X7-4L8jRCD',
+        'etV*p%Nr6w&H%FeF',
+      ],
+      invalid: [
+        '',
+        'password',
+        'hunter2',
+        'hello world',
+        'passw0rd',
+        'password!',
+        'PASSWORD!',
+      ],
+    });
+  });
+
   it('should validate base64URL', () => {
     test({
       validator: 'isBase64',
