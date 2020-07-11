243c5706bca06f000ffb116ad2647bab9c4dc9c2
Sarhan <sarhan@anava.tn>
2020-07-11 11:26:08 +0100

    fix: use regex flag to simplify isSemVer validator

--- es/lib/isSemVer.js
+++ es/lib/isSemVer.js
@@ -7,7 +7,7 @@ import multilineRegexp from './util/multilineRegex';
  * Reference: https://semver.org/
  */
 
-var semanticVersioningRegex = multilineRegexp(['^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)', '(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))', '?(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?$']);
+var semanticVersioningRegex = multilineRegexp(['^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)', '(?:-((?:0|[1-9]\\d*|\\d*[a-z-][0-9a-z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-z-][0-9a-z-]*))*))', '?(?:\\+([0-9a-z-]+(?:\\.[0-9a-z-]+)*))?$'], 'i');
 export default function isSemVer(str) {
   assertString(str);
   return semanticVersioningRegex.test(str);
