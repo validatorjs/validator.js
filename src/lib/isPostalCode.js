279e771e8f891ad70186e9d43cbc39dd0c503637
Rubin Bhandari <roobin.bhandari@gmail.com>
2020-10-06 04:26:21 +0545

    feat(isPostalCode): add belarus locale (#1459)
    * added postalcode for belarus
    
    * test updated

--- src/lib/isPostalCode.js
+++ src/lib/isPostalCode.js
@@ -14,6 +14,7 @@ const patterns = {
   BE: fourDigit,
   BG: fourDigit,
   BR: /^\d{5}-\d{3}$/,
+  BY: /2[1-4]{1}\d{4}$/,
   CA: /^[ABCEGHJKLMNPRSTVXY]\d[ABCEGHJ-NPRSTV-Z][\s\-]?\d[ABCEGHJ-NPRSTV-Z]\d$/i,
   CH: fourDigit,
   CZ: /^\d{3}\s?\d{2}$/,
