027da259493b24a172090076eea1eb5b031ae139
rubiin <roobin.bhandari@gmail.com>
2020-10-08 20:52:37 +0545

    deleted build files

--- es/lib/isBIC.js
+++
@@ -1,6 +0,0 @@
-import assertString from './util/assertString';
-var isBICReg = /^[A-z]{4}[A-z]{2}\w{2}(\w{3})?$/;
-export default function isBIC(str) {
-  assertString(str);
-  return isBICReg.test(str);
-}
--- es/lib/unescape.js
+++
@@ -1,5 +0,0 @@
-import assertString from './util/assertString';
-export default function unescape(str) {
-  assertString(str);
-  return str.replace(/&amp;/g, '&').replace(/&quot;/g, '"').replace(/&#x27;/g, "'").replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&#x2F;/g, '/').replace(/&#x5C;/g, '\\').replace(/&#96;/g, '`');
-}
