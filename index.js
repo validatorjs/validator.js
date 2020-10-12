5047f208d9765361c941ded7a1c34b93c6c6a175
Tanate Meaksriswan <tanatezp@gmail.com>
2020-10-12 18:30:25 +0700

    Remove unrelated changes

--- index.js
+++ index.js
@@ -149,6 +149,8 @@ var _isISO31661Alpha2 = _interopRequireDefault(require("./lib/isISO31661Alpha3")
 
 var _isBase = _interopRequireDefault(require("./lib/isBase32"));
 
+var _isBase2 = _interopRequireDefault(require("./lib/isBase64"));
+
 var _isDataURI = _interopRequireDefault(require("./lib/isDataURI"));
 
 var _isMagnetURI = _interopRequireDefault(require("./lib/isMagnetURI"));
@@ -265,6 +267,7 @@ var validator = {
   isISO31661Alpha2: _isISO31661Alpha.default,
   isISO31661Alpha3: _isISO31661Alpha2.default,
   isBase32: _isBase.default,
+  isBase64: _isBase2.default,
   isDataURI: _isDataURI.default,
   isMagnetURI: _isMagnetURI.default,
   isMimeType: _isMimeType.default,
