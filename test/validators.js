19baf6d453c2c5f1d9bbb5e4a80e1f32c29eced5
Timothy Beck <socaltdb@gmail.com>
2020-11-17 14:49:21 -0800

    Add default args for clarity

--- test/validators.js
+++ test/validators.js
@@ -8564,6 +8564,13 @@ describe('Validators', () => {
   it('should validate strong passwords', () => {
     test({
       validator: 'isStrongPassword',
+      args: [{
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      }],
       valid: [
         '%2%k{7BsL"M%Kd6e',
         'EXAMPLE of very long_password123!',
