727cdf01f1633681c7b02dddffbaee051fc1a99f
httpsbao <httpsbao@outlook.com>
2020-11-28 23:36:19 +0800

    discard changes to auto-generated files (index.js, validator.js,validator.min.js)

--- index.js
+++ index.js
@@ -185,8 +185,6 @@ var _normalizeEmail = _interopRequireDefault(require("./lib/normalizeEmail"));
 
 var _isSlug = _interopRequireDefault(require("./lib/isSlug"));
 
-var _isStrongPassword = _interopRequireDefault(require("./lib/isStrongPassword"));
-
 function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
@@ -289,7 +287,6 @@ var validator = {
   normalizeEmail: _normalizeEmail.default,
   toString: toString,
   isSlug: _isSlug.default,
-  isStrongPassword: _isStrongPassword.default,
   isTaxID: _isTaxID.default,
   isDate: _isDate.default
 };
