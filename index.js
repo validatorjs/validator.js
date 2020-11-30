97771fef7ab1e629383bdd64f0d45d3d79c65ea1
Federico Ciardi <fed.ciardi@gmail.com>
2020-11-30 15:07:31 +0100

    fix merge conflicts

--- index.js
+++ index.js
@@ -195,7 +195,7 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var version = '13.5.0';
+var version = '13.5.1';
 var validator = {
   version: version,
   toDate: _toDate.default,
