20f7a54eb715081a7166d894305bde907d44ab1b
Federico Ciardi <fed.ciardi@gmail.com>
2020-11-30 12:21:25 +0100

    feat(isVAT): add Italian regex

--- src/lib/isVAT.js
+++ src/lib/isVAT.js
@@ -2,6 +2,7 @@ import assertString from './util/assertString';
 
 export const vatMatchers = {
   GB: /^GB((\d{3} \d{4} ([0-8][0-9]|9[0-6]))|(\d{9} \d{3})|(((GD[0-4])|(HA[5-9]))[0-9]{2}))$/,
+  IT: /^(IT)?[0-9]{11}$/,
 };
 
 export default function isVAT(str, countryCode) {
