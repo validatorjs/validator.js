467deb81141f77d72acf8efd9452f3716560c6a2
Leon Schwanitz <leon@28minds.com>
2020-12-11 14:09:48 +0100

    Correct length for german vat

--- validator.js
+++ validator.js
@@ -4618,7 +4618,7 @@ function isStrongPassword(str) {
 var vatMatchers = {
   GB: /^GB((\d{3} \d{4} ([0-8][0-9]|9[0-6]))|(\d{9} \d{3})|(((GD[0-4])|(HA[5-9]))[0-9]{2}))$/,
   IT: /^(IT)?[0-9]{11}$/,
-  DE: /^(DE)?[0-9]{11}$/
+  DE: /^(DE)?[0-9]{9}$/
 };
 function isVAT(str, countryCode) {
   assertString(str);
