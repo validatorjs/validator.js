7c485b0e3045f35726eed1c1a2260930e42811f2
Leon Schwanitz <leon@28minds.com>
2020-12-11 14:04:15 +0100

    Added german VAT Validation

--- validator.js
+++ validator.js
@@ -4617,7 +4617,8 @@ function isStrongPassword(str) {
 
 var vatMatchers = {
   GB: /^GB((\d{3} \d{4} ([0-8][0-9]|9[0-6]))|(\d{9} \d{3})|(((GD[0-4])|(HA[5-9]))[0-9]{2}))$/,
-  IT: /^(IT)?[0-9]{11}$/
+  IT: /^(IT)?[0-9]{11}$/,
+  DE: /^(DE)?[0-9]{11}$/
 };
 function isVAT(str, countryCode) {
   assertString(str);
