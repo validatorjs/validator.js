edcd1618bfdb5a6b06b18123f0b9d9fcdeedca65
Michael Firlus <michael@4lus.de>
2020-12-17 11:02:24 +0100

    feat(isLicensePlate): new validator 🎉 (#1495)
    * Added isLicensePlate check with german locale
    
    * de-LI (Liechtenstein) validator
    
    * Added albanian number plates
    
    * reversed changes in index.js
    
    * reversed changes in index.js
    
    * Refactored de-DE into one big regex, expanded tests fot de-DE
    
    * added pt-PT locale
    
    * Fixed coverage problem
    
    * feat(isLicensePlate): clean build
    
    * coverage is now 100%
    
    * Update README.md
    
    * Update README.md

--- test/validators.js
+++ test/validators.js
@@ -10209,7 +10209,111 @@ describe('Validators', () => {
       ],
     });
   });
-
+  it('should be valid license plate', () => {
+    test({
+      validator: 'isLicensePlate',
+      args: ['pt-PT'],
+      valid: [
+        'AA-12-34',
+        '12·34·AB',
+        '12·AB·34',
+        'AB 12 CD',
+        'AB12CD',
+      ],
+      invalid: [
+        '',
+        'notalicenseplate',
+        'A1-B2-C3',
+        'ABC-1-EF',
+      ],
+    });
+    test({
+      validator: 'isLicensePlate',
+      args: ['de-LI'],
+      valid: [
+        'FL 1',
+        'FL 99999',
+        'FL 1337',
+      ],
+      invalid: [
+        '',
+        'FL 999999',
+        'AB 12345',
+        'FL -1',
+      ],
+    });
+    test({
+      validator: 'isLicensePlate',
+      args: ['de-DE'],
+      valid: [
+        'M A 1',
+        'M A 12',
+        'M A 123',
+        'M A 1234',
+        'M AB 1',
+        'M AB 12',
+        'M AB 123',
+        'M AB 1234',
+        'FS A 1',
+        'FS A 12',
+        'FS A 123',
+        'FS A 1234',
+        'FS AB 1',
+        'FS AB 12',
+        'FS AB 123',
+        'FS AB 1234',
+        'FSAB1234',
+        'FS-AB-1234',
+        'FS AB 1234 H',
+        'FS AB 1234 E',
+        'FSAB1234E',
+        'FS-AB-1234-E',
+      ],
+      invalid: [
+        'YY AB 123',
+        'PAF AB 1234',
+        'M ABC 123',
+        'M AB 12345',
+        'FS AB 1234 A',
+      ],
+    });
+    test({
+      validator: 'isLicensePlate',
+      args: ['sq-AL'],
+      valid: [
+        'AA 000 AA',
+        'ZZ 999 ZZ',
+      ],
+      invalid: [
+        '',
+        'AA 0 A',
+        'AAA 00 AAA',
+      ],
+    });
+    test({
+      validator: 'isLicensePlate',
+      args: ['any'],
+      valid: [
+        'FL 1',
+        'FS AB 123',
+      ],
+      invalid: [
+        '',
+        'FL 999999',
+        'FS AB 1234 A',
+      ],
+    });
+    test({
+      validator: 'isLicensePlate',
+      args: ['asdfasdf'],
+      error: [
+        'FL 1',
+        'FS AB 123',
+        'FL 999999',
+        'FS AB 1234 A',
+      ],
+    });
+  });
   it('should validate english VAT numbers', () => {
     test({
       validator: 'isVAT',
