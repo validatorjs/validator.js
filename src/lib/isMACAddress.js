389b5ef06b573d0ff5e613c0e2fc769e9d459f9b
Federico Ciardi <fed.ciardi@gmail.com>
2021-02-24 11:48:43 +0100

    fix(isMacAddress): improve Regexes

--- src/lib/isMACAddress.js
+++ src/lib/isMACAddress.js
@@ -1,19 +1,18 @@
 import assertString from './util/assertString';
 
-const macAddress = /^([0-9a-fA-F][0-9a-fA-F]:){5}([0-9a-fA-F][0-9a-fA-F])$/;
-const macAddressNoColons = /^([0-9a-fA-F]){12}$/;
-const macAddressWithHyphen = /^([0-9a-fA-F][0-9a-fA-F]-){5}([0-9a-fA-F][0-9a-fA-F])$/;
-const macAddressWithSpaces = /^([0-9a-fA-F][0-9a-fA-F]\s){5}([0-9a-fA-F][0-9a-fA-F])$/;
-const macAddressWithDots = /^([0-9a-fA-F]{4}).([0-9a-fA-F]{4}).([0-9a-fA-F]{4})$/;
+const macAddress = /^(?:[0-9a-fA-F]{2}([-:\s]))([0-9a-fA-F]{2}\1){4}([0-9a-fA-F]{2})$/;
+const macAddressNoSeparator = /^([0-9a-fA-F]){12}$/;
+const macAddressWithDots = /^([0-9a-fA-F]{4}\.){2}([0-9a-fA-F]{4})$/;
 
 export default function isMACAddress(str, options) {
   assertString(str);
-  if (options && options.no_colons) {
-    return macAddressNoColons.test(str);
+  /**
+   * @deprecated `no_colons` deprecated
+  */
+  if (options && (options.no_colons || options.noSeparator)) {
+    return macAddressNoSeparator.test(str);
   }
 
   return macAddress.test(str)
-    || macAddressWithHyphen.test(str)
-    || macAddressWithSpaces.test(str)
     || macAddressWithDots.test(str);
 }
