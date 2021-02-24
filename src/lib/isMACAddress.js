91817a826f90fc7490e502ecd56764975a6ba600
Federico Ciardi <fed.ciardi@gmail.com>
2021-02-24 12:46:47 +0100

    docs: update docs

--- src/lib/isMACAddress.js
+++ src/lib/isMACAddress.js
@@ -1,7 +1,7 @@
 import assertString from './util/assertString';
 
 const macAddress = /^(?:[0-9a-fA-F]{2}([-:\s]))([0-9a-fA-F]{2}\1){4}([0-9a-fA-F]{2})$/;
-const macAddressNoSeparator = /^([0-9a-fA-F]){12}$/;
+const macAddressNoSeparators = /^([0-9a-fA-F]){12}$/;
 const macAddressWithDots = /^([0-9a-fA-F]{4}\.){2}([0-9a-fA-F]{4})$/;
 
 export default function isMACAddress(str, options) {
@@ -9,8 +9,8 @@ export default function isMACAddress(str, options) {
   /**
    * @deprecated `no_colons` deprecated
   */
-  if (options && (options.no_colons || options.noSeparator)) {
-    return macAddressNoSeparator.test(str);
+  if (options && (options.no_colons || options.noSeparators)) {
+    return macAddressNoSeparators.test(str);
   }
 
   return macAddress.test(str)
