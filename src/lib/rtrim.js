6d87bfe20c542969e1c12a1f0c1f8979c3b97ff1
Federico Ciardi <fed.ciardi@gmail.com>
2021-03-03 11:49:47 +0100

    fix(isSlug & rtrim): regex no longer exposed to ReDOS attacks (#1603)

--- src/lib/rtrim.js
+++ src/lib/rtrim.js
@@ -3,6 +3,6 @@ import assertString from './util/assertString';
 export default function rtrim(str, chars) {
   assertString(str);
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#Escaping
-  const pattern = chars ? new RegExp(`[${chars.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}]+$`, 'g') : /\s+$/g;
+  const pattern = chars ? new RegExp(`[${chars.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}]+$`, 'g') : /(\s)+$/g;
   return str.replace(pattern, '');
 }
