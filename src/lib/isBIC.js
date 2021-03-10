cfb6be2ae1a2b184fc1674be769bc35637b06428
Bruce MacNaughton <bruce.macnaughton@contrastsecurity.com>
2021-03-09 18:46:01 -0800

    Use correct BIC regex

--- src/lib/isBIC.js
+++ src/lib/isBIC.js
@@ -1,6 +1,7 @@
 import assertString from './util/assertString';
 
-const isBICReg = /^[A-z]{4}[A-z]{2}\w{2}(\w{3})?$/;
+// https://en.wikipedia.org/wiki/ISO_9362
+const isBICReg = /^[A-Z]{6}[A-Z0-9]{2}([A-Z0-9]{3})?$/;
 
 export default function isBIC(str) {
   assertString(str);
