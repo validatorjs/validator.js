d2a7285ce5f7b52bd925f48fc56506961995a7db
greatSumini <greatsumini@gmail.com>
2021-03-15 21:54:23 +0900

    Add KR (SouthKorea) postalCode validator and test.

--- test/validators.js
+++ test/validators.js
@@ -9503,6 +9503,17 @@ describe('Validators', () => {
           'ab1234',
         ],
       },
+      {
+        locale: 'KR',
+        valid: [
+          '17008',
+          '339012',
+        ],
+        invalid: [
+          '1412347',
+          'ab1234',
+        ],
+      },
     ];
 
     let allValid = [];
