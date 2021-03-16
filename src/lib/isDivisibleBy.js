1daa4787be5aeec7b0b11adf2bf033d4b9738f34
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2021-03-15 17:45:31 -0700

    don't let arbitrary characters through
    - parseInt stops at first invalid character, so no real validation.

--- src/lib/isDivisibleBy.js
+++ src/lib/isDivisibleBy.js
@@ -1,7 +1,11 @@
 import assertString from './util/assertString';
 import toFloat from './toFloat';
+import isInt from './isInt';
 
 export default function isDivisibleBy(str, num) {
   assertString(str);
+  if (num.constructor !== Number && !isInt(num)) {
+    return false;
+  }
   return toFloat(str) % parseInt(num, 10) === 0;
 }
--- src/lib/toInt.js
+++ src/lib/toInt.js
@@ -1,6 +1,21 @@
 import assertString from './util/assertString';
+import isFloat from './isFloat';
 
-export default function toInt(str, radix) {
+export default function toInt(str, radix = 10) {
   assertString(str);
-  return parseInt(str, radix || 10);
+  str = str.trim();
+  if (!isFloat(str)) {
+    if (radix <= 10) {
+      return NaN;
+    }
+    // it's a radix higher than 10
+    str = str.toLowerCase();
+    const base = radix - 11;
+    const high = 'a'.charCodeAt(0) + base;
+    const re = new RegExp(`^[-+]?[0-9a-${String.fromCharCode(high)}]+`);
+    if (!re.test(str)) {
+      return NaN;
+    }
+  }
+  return parseInt(str, radix);
 }
