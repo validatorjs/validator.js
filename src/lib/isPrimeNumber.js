e17c06d54bc4699ead41d63d63d531a5abbd16b2
harshsingh25 <harshaiactr@gmail.com>
2021-08-31 16:59:23 +0530

    isprimenumber fix

--- src/lib/isPrimeNumber.js
+++ src/lib/isPrimeNumber.js
@@ -2,7 +2,7 @@ import assertString from './util/assertString';
 
 export default function isPrimeNumber(str) {
   assertString(str);
-
+  let n = str;
   if (isNaN(n) || !isFinite(n) || n % 1 || n < 2) return false;
   var m = Math.sqrt(n); 
   for (var i = 2; i <= m; i++)
