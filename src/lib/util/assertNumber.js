8015b1eabf08c320e8d3b8bca9b6b515b3a2bef6
salah alhashmi <alhashmis246@gmail.com>
2021-09-19 02:26:36 -0700

    solve singlequots error and trailing line

--- src/lib/util/assertNumber.js
+++ src/lib/util/assertNumber.js
@@ -1,10 +1,10 @@
 export default function assertNumber(input) {
-  const isNumber = typeof input === "number" || input instanceof Number;
+  const isNumber = typeof input === 'number' || input instanceof Number;
 
   if (!isNumber) {
     let invalidType = typeof input;
-    if (input === null) invalidType = "null";
-    else if (invalidType === "object") invalidType = input.constructor.name;
+    if (input === null) invalidType = 'null';
+    else if (invalidType === 'object') invalidType = input.constructor.name;
 
     throw new TypeError(`Expected a number but received a ${invalidType}`);
   }
