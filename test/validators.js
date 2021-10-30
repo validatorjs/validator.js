622184e669162453e155499417e1a922f548e810
Andrea Fassina <fasenderos@gmail.com>
2021-10-30 07:56:32 +0200

    feat(isFQDN): add `allow_wildcard` option (#1647)
    Co-authored-by: Andrea Fassina <andrea.fassina@nativery.com>

--- test/validators.js
+++ test/validators.js
@@ -1118,6 +1118,18 @@ describe('Validators', () => {
       ],
     });
   });
+  it('should validate FQDN with wildcard option', () => {
+    test({
+      validator: 'isFQDN',
+      args: [
+        { allow_wildcard: true },
+      ],
+      valid: [
+        '*.example.com',
+        '*.shop.example.com',
+      ],
+    });
+  });
 
   it('should validate alpha strings', () => {
     test({
