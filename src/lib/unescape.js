d799421ce332cd689da5a6c42fe2447e7dbf51fc
Markus <markus.tyrkko@gmail.com>
2021-10-30 10:28:15 +0300

    Added reference to issue

--- src/lib/unescape.js
+++ src/lib/unescape.js
@@ -10,4 +10,7 @@ export default function unescape(str) {
     .replace(/&#x5C;/g, '\\')
     .replace(/&#96;/g, '`')
     .replace(/&amp;/g, '&'));
+  // &amp; replacement has to be the last one to prevent
+  // bugs with intermediate strings containing escape sequences
+  // See: https://github.com/validatorjs/validator.js/issues/1827
 }
