420562186bfa031798fc7c42b3fe8bcb5b236502
Sanel Hadzini <31168388+theteladras@users.noreply.github.com>
2021-10-30 22:48:51 +0200

    improve(isUUID) validation for version 1 and 2

--- test/validators.js
+++ test/validators.js
@@ -4531,6 +4531,35 @@ describe('Validators', () => {
     });
     test({
       validator: 'isUUID',
+      args: [1],
+      valid: [
+        'E034B584-7D89-11E9-9669-1AECF481A97B',
+      ],
+      invalid: [
+        'xxxA987FBC9-4BED-3078-CF07-9141BA07C9F3',
+        'AAAAAAAA-1111-2222-AAAG',
+        'AAAAAAAA-1111-2222-AAAG-111111111111',
+        'A987FBC9-4BED-4078-8F07-9141BA07C9F3',
+        'A987FBC9-4BED-5078-AF07-9141BA07C9F3',
+      ],
+    });
+    test({
+      validator: 'isUUID',
+      args: [2],
+      valid: [
+        'A987FBC9-4BED-2078-CF07-9141BA07C9F3',
+      ],
+      invalid: [
+        '',
+        'xxxA987FBC9-4BED-3078-CF07-9141BA07C9F3',
+        '11111',
+        'AAAAAAAA-1111-1111-AAAG-111111111111',
+        'A987FBC9-4BED-4078-8F07-9141BA07C9F3',
+        'A987FBC9-4BED-5078-AF07-9141BA07C9F3',
+      ],
+    });
+    test({
+      validator: 'isUUID',
       args: [3],
       valid: [
         'A987FBC9-4BED-3078-CF07-9141BA07C9F3',
@@ -4578,6 +4607,18 @@ describe('Validators', () => {
         'AAAAAAAA-1111-1111-AAAG-111111111111',
         '9c858901-8a57-4791-81fe-4c455b099bc9',
         'A987FBC9-4BED-3078-CF07-9141BA07C9F3',
+      ],
+    });
+    test({
+      validator: 'isUUID',
+      args: [6],
+      valid: [],
+      invalid: [
+        '987FBC97-4BED-1078-AF07-9141BA07C9F3',
+        '987FBC97-4BED-2078-AF07-9141BA07C9F3',
+        '987FBC97-4BED-3078-AF07-9141BA07C9F3',
+        '987FBC97-4BED-4078-AF07-9141BA07C9F3',
+        '987FBC97-4BED-5078-AF07-9141BA07C9F3',
       ],
     });
   });
