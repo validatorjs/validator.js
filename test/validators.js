60b6cf0757fa7cd3eaa5e92c59fbba9f51f34a51
Dave Borghuis <dave@daveborghuis.nl>
2021-10-30 12:01:48 +0200

    feat(isVAT): add dutch NL locale (#1825)
    for hacktober #1742
    
    if you want to extend it more check :
    https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s21.html

--- test/validators.js
+++ test/validators.js
@@ -11351,7 +11351,7 @@ describe('Validators', () => {
       ],
     });
   });
-  it('should validate english VAT numbers', () => {
+  it('should validate VAT numbers', () => {
     test({
       validator: 'isVAT',
       args: ['GB'],
@@ -11381,7 +11381,6 @@ describe('Validators', () => {
         'GBHA499',
       ],
     });
-
     test({
       validator: 'isVAT',
       args: ['IT'],
@@ -11397,7 +11396,21 @@ describe('Validators', () => {
         'IT123456789',
       ],
     });
-
+    test({
+      validator: 'isVAT',
+      args: ['NL'],
+      valid: [
+        'NL123456789B10',
+        '123456789B10',
+      ],
+      invalid: [
+        'NL12345678 910',
+        'NL 123456789101',
+        'NL123456789B1',
+        'GB12345678910',
+        'NL123456789',
+      ],
+    });
     test({
       validator: 'isVAT',
       args: ['invalidCountryCode'],
