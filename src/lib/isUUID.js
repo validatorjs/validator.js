af2b43c769a3bb9013fe12f3f3747f8124756664
Sanel Hadzini <31168388+theteladras@users.noreply.github.com>
2021-10-31 06:14:16 +0100

    feat(isUUID): add support for validation of version v1 and v2 (#1848)
    * fix(isUUID) for null version argument supply
    
    * improve(isUUID) validation for version 1 and 2

--- src/lib/isUUID.js
+++ src/lib/isUUID.js
@@ -1,6 +1,8 @@
 import assertString from './util/assertString';
 
 const uuid = {
+  1: /^[0-9A-F]{8}-[0-9A-F]{4}-1[0-9A-F]{3}-[0-9A-F]{4}-[0-9A-F]{12}$/i,
+  2: /^[0-9A-F]{8}-[0-9A-F]{4}-2[0-9A-F]{3}-[0-9A-F]{4}-[0-9A-F]{12}$/i,
   3: /^[0-9A-F]{8}-[0-9A-F]{4}-3[0-9A-F]{3}-[0-9A-F]{4}-[0-9A-F]{12}$/i,
   4: /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i,
   5: /^[0-9A-F]{8}-[0-9A-F]{4}-5[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i,
@@ -10,5 +12,5 @@ const uuid = {
 export default function isUUID(str, version) {
   assertString(str);
   const pattern = uuid[![undefined, null].includes(version) ? version : 'all'];
-  return pattern && pattern.test(str);
+  return !!pattern && pattern.test(str);
 }
