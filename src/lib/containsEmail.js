37827ba5c39882caea8464d0ab59de0d17df599e
masihbr <masihbr@gmail.com>
2021-12-17 14:07:55 +0330

    improve of containsEmail using better algorithm

--- src/lib/containsEmail.js
+++ src/lib/containsEmail.js
@@ -1,15 +1,53 @@
 import assertString from './util/assertString';
 import isEmail from './isEmail';
 
-export default function containsEmail(str) {
-  assertString(str);
-  let n = str.length;
-  for (let i = 0; i < n; i++) {
-    for (let j = i + 1; j <= n; j++) {
-      if (isEmail(str.substring(i, j))) {
-        return true;
-      }
+function getAtSignIndices(str) {
+  let indices = [];
+  for (let i = 1; i < str.length - 1; i++) {
+    // @ in the beginning or end can't make a valid email address.
+    // two @ next to each other can't make a valid email address.
+    if (str[i] === '@' && str[i + 1] !== '@' && str[i - 1] !== '@') {
+      indices.push(i);
     }
   }
+  return indices;
+}
+
+function checkAtSignLeftSide(atSignIndex, str) {
+  for (let i = atSignIndex - 1; i >= 0; i--) {
+    if (isEmail(`${str.substring(i, atSignIndex)}@valid.com`)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function checkAtSignRightSide(atSignIndex, str) {
+  for (let i = atSignIndex + 2; i <= str.length; i++) {
+    if (isEmail(`valid@${str.substring(atSignIndex + 1, i)}`)) {
+      return true;
+    }
+    if (i >= str.length || str.charAt(i) === '@') {
+      break;
+    }
+  }
+  return false;
+}
+
+export default function containsEmail(str) {
+  assertString(str);
+  if (!str.includes('@')) {
+    return false;
+  }
+
+  let atSignIndices = getAtSignIndices(str);
+  for (const atSignIndex of atSignIndices) {
+    let isLeftSideValid = checkAtSignLeftSide(atSignIndex, str);
+    let isRightSideValid = checkAtSignRightSide(atSignIndex, str);
+    if (isLeftSideValid && isRightSideValid) {
+      return true;
+    }
+  }
+
   return false;
 }
