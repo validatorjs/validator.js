605fb8e389bbe7ed977453afe5a4b1843b48f11f
Caleb Adewole <caleberioluwa@gmail.com>
2022-01-04 20:00:39 +0100

    add isvalidsiunit validator

---
+++ src/lib/isValidSIUnit.js
@@ -0,0 +1,17 @@
+// "http://physics.nist.gov/cuu/Units/units.html"
+const units = [
+  'kat/m³', 'W/(m²·sr)', 'W/sr', 'Gy/s', 'C/kg', 'J/(mol·K)', 'J/mol',
+  'H/m', 'F/m', 'C/m²', 'C/m³', 'V/m', 'J/m³', 'W/(m·K)', 'J/kg', 'J/(kg·K)',
+  'J/K', 'W/m²', 'rad/s²', 'rad/s', 'N/m', 'N·m', 'Pa·s', 'kat', 'Sv', 'Gy', 'Bq',
+  'lx', 'lm', 'H', 'T', 'Wb', 'S', 'Ω', 'V', 'C', 'W', 'J', 'Pa', 'N', 'Hz', 'sr', 'rad',
+  'cd/m²', 'mol/m³', 'A/m', 'A/m²', 'm³/kg', 'kg/m³', 'm⁻¹', 'm/s²', 'm/s', 'm³', 'm²',
+  'cd', 'mol', 'K', 'A', 's', 'kg', 'm',
+];
+
+const unitSet = new Set(units);
+function isValidSIUnit(str) {
+  str = typeof str !== 'string' ? null : str;
+  if (str === null || str === undefined) { return false; }
+  return unitSet.has(str);
+}
+export default isValidSIUnit;
