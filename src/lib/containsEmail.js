0716468454d5fd9e356a0537bf9642d159868e36
Emily Jones <58923694+emilyjonesr@users.noreply.github.com>
2022-01-25 01:41:45 -0500

    fixed syntax

--- src/lib/containsEmail.js
+++ src/lib/containsEmail.js
@@ -1,19 +1,22 @@
 import isEmail from './isEmail';
 
 export default function containsEmail(str) {
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] == '@') {
-            let l = i-1;
-            let r = i+1;
-            while (l != 0 || r != str.length) {
-                if (isEmail(str.substr(l,r)))
-                    return true;
-                if (l != 0)
-                    l--;
-                if (r != str.length)
-                    r++;
-            }
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '@') {
+      let l = i - 1;
+      let r = i + 1;
+      while (l !== 0 || r !== str.length) {
+        if (isEmail(str.substr(l, r))) {
+          return true;
         }
+        if (l !== 0) {
+          l = l + 1;
+        }
+        if (r !== str.length) {
+          r = r + 1;
+        }
+      }
     }
-    return false;
+  }
+  return false;
 }
