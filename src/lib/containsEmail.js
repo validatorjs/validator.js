deda36d5d518c76e45ac8574ff717e8555b124a3
Emily Jones <58923694+emilyjonesr@users.noreply.github.com>
2022-01-30 23:05:19 -0500

    checks both as it moves left and right

--- src/lib/containsEmail.js
+++ src/lib/containsEmail.js
@@ -3,20 +3,25 @@ import isEmail from './isEmail';
 export default function containsEmail(str) {
   for (let i = 0; i < str.length; i++) {
     if (str[i] === '@') {
-      let l = i - 1;
-      let r = i + 1;
+      let l = i;
+      let r = i;
+
       while (l !== 0 || r !== str.length) {
-        if (isEmail(str.substr(l, r))) {
-          return true;
-        }
         if (l !== 0) {
           l -= 1;
+          if (isEmail(str.substr(l, r))) {
+            return true;
+          }
         }
         if (r !== str.length) {
           r += 1;
+          if (isEmail(str.substr(l, r))) {
+            return true;
+          }
         }
       }
     }
   }
+  
   return false;
 }
