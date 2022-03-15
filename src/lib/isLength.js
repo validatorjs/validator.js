c814f9ec5ee4bf2d91075a51f932f56650fa8141
Carlos Daniel Gaxiola Gaxiola <carlos.gaxiola@MacBook-Pro-de-Carlos.local>
2022-03-15 09:26:40 -0700

    Moved the feature as a complement of isLength feature

--- src/lib/isLength.js
+++ src/lib/isLength.js
@@ -1,18 +1,32 @@
-import assertString from './util/assertString';
+import assertString from './assertString';
 
 /* eslint-disable prefer-rest-params */
 export default function isLength(str, options) {
   assertString(str);
   let min;
   let max;
+  let exact;
+  let match = false;
   if (typeof (options) === 'object') {
     min = options.min || 0;
     max = options.max;
+    exact = options.exact
   } else { // backwards compatibility: isLength(str, min [, max])
     min = arguments[1] || 0;
     max = arguments[2];
+    exact = arguments[3];
+
   }
   const surrogatePairs = str.match(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g) || [];
   const len = str.length - surrogatePairs.length;
-  return len >= min && (typeof max === 'undefined' || len <= max);
+  if (exact && (len >= min && (typeof max === 'undefined' || len <= max))) {
+    exact.forEach(element => {
+      if(len === element){
+        match = true;
+      }
+    })
+  } else {
+    return len >= min && (typeof max === 'undefined' || len <= max);
+  }
+  return match;
 }
