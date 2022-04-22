b03920261f41fb6bd574c3ce38e4b7da344d6710
Brage Sekse Aarset <brage@bjerk.io>
2022-04-22 06:06:10 +0300

    feat(isISO6391): add ISO 639-1 validator (#1892)
    * docs: add ISO 639-1 to readme
    
    * feat: implement ISO 639-1 validator and test
    
    * update readme
    
    * fix: clean up whitespace in locale list
    
    * refactor: write more concise test code

--- test/validators.js
+++ test/validators.js
@@ -9792,6 +9792,14 @@ describe('Validators', () => {
     });
   });
 
+  it('should validate ISO 639-1 language codes', () => {
+    test({
+      validator: 'isISO6391',
+      valid: ['ay', 'az', 'ba', 'be', 'bg'],
+      invalid: ['aj', 'al', 'pe', 'pf', 'abc', '123', ''],
+    });
+  });
+
   const validISO8601 = [
     '2009-12T12:34',
     '2009',
