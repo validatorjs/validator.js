cf8dcea2292571e19aa968cad11ddc4976089e18
Oswaldo Vázquez <bigosvaap@gmail.com>
2022-04-21 20:57:53 -0600

    feat(isBase32): Add option for Crockford's base 32 alternative (#1888)
    closes #1857 
    
    * feat(crockforbase32): Add option for Crockford's base 32 alternative to isBase32 method
    
    * test move out to its own it() statement
    
    * Update README.md
    
    Co-authored-by: Brage <brage.aarset@gmail.com>
    
    Co-authored-by: Brage <brage.aarset@gmail.com>

--- test/validators.js
+++ test/validators.js
@@ -5753,6 +5753,25 @@ describe('Validators', () => {
     });
   });
 
+  it('should validate base32 strings with crockford alternative', () => {
+    test({
+      validator: 'isBase32',
+      args: [{ crockford: true }],
+      valid: [
+        '91JPRV3F41BPYWKCCGGG',
+        '60',
+        '64',
+        'B5QQA833C5Q20S3F41MQ8',
+      ],
+      invalid: [
+        '91JPRV3F41BUPYWKCCGGG',
+        'B5QQA833C5Q20S3F41MQ8L',
+        '60I',
+        'B5QQA833OULIC5Q20S3F41MQ8',
+      ],
+    });
+  });
+
   it('should validate base58 strings', () => {
     test({
       validator: 'isBase58',
