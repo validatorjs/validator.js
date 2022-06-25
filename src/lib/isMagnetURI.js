369a79c53b7fb6b76f5fc239dfba9aa4364f998a
Rhilip <rhilipruan@gmail.com>
2022-06-25 09:19:58 +0800

    fix(isMagnetURI): make sure `xt=` is uri component

--- src/lib/isMagnetURI.js
+++ src/lib/isMagnetURI.js
@@ -1,6 +1,6 @@
 import assertString from './util/assertString';
 
-const magnetURIComponent = /xt(?:\.1)?=urn:((?:aich|bitprint|btih|ed2k|ed2khash|kzhash|md5|sha1|tree:tiger):[a-z0-9]{32}(?:[a-z0-9]{8})?|btmh:1220[a-z0-9]{64})($|&)/i;
+const magnetURIComponent = /[?&]xt(?:\.1)?=urn:((?:aich|bitprint|btih|ed2k|ed2khash|kzhash|md5|sha1|tree:tiger):[a-z0-9]{32}(?:[a-z0-9]{8})?|btmh:1220[a-z0-9]{64})($|&)/i;
 
 export default function isMagnetURI(url) {
   assertString(url);
