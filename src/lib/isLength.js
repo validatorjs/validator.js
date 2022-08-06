ae711d6c6d1da5f2f90107b5f9f2b0e7710eb753
Vatsal <bevatsal1122@gmail.com>
2022-08-07 00:05:03 +0530

    Added Exact Condition for String Length & Updated README.md

--- src/lib/isLength.js
+++ src/lib/isLength.js
@@ -1,19 +1,45 @@
-import assertString from './util/assertString';
+import assertString from './util/assertString.js';
 
 /* eslint-disable prefer-rest-params */
 export default function isLength(str, options) {
   assertString(str);
   let min;
   let max;
+  let exact;
+  let result;
+  let isPerfect = false;
   if (typeof (options) === 'object') {
     min = options.min || 0;
     max = options.max;
+    exact = options.exact;
   } else { // backwards compatibility: isLength(str, min [, max])
     min = arguments[1] || 0;
     max = arguments[2];
+    exact = arguments[3];
   }
   const presentationSequences = str.match(/(\uFE0F|\uFE0E)/g) || [];
   const surrogatePairs = str.match(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g) || [];
   const len = str.length - presentationSequences.length - surrogatePairs.length;
-  return len >= min && (typeof max === 'undefined' || len <= max);
+  result = len >= min && (typeof max === 'undefined' || len <= max);
+  if (result === false || typeof exact === 'undefined') return result;
+  if (Array.isArray(exact) === true) {
+    for (let oneElem of exact) {
+      if (len === oneElem) {
+        isPerfect = true;
+        break;
+      }
+    }
+  } else if (typeof exact === 'number') {
+    if (len === exact) {
+      isPerfect = true;
+    }
+  } else if (typeof exact === 'object') {
+    for (let key in exact) {
+      if (len === exact[key]) {
+        isPerfect = true;
+        break;
+      }
+    }
+  }
+  return isPerfect;
 }
