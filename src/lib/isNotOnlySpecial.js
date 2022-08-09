5465be5dc61820aba41de5f289c98a17756ae159
Akshat <akshatm408@gmail.com>
2022-08-09 19:18:50 +0530

    Fixed extra spaces, and added a new line

--- src/lib/isNotOnlySpecial.js
+++ src/lib/isNotOnlySpecial.js
@@ -1,16 +1,6 @@
 import assertString from './util/assertString';
-/**
- * Checks to see whether the input string is not made up of just special characters
- * 
- */
-export default function isNotOnlySpecial(str) {
-    const result = str.match(/^[A-Z#@,]+$/);
-    assertString(str);
-    if (result) {
-        return true;
-    }
-    else {
-        return false;
-    }
 
+export default function isNotOnlySpecial(str) {
+    assertString(str);
+    return str.match(/^[A-Z#@,]+$/) || false;
 }
