18e5c72ab030532203859b863230447ac71ed80a
Michele D'Amico <michele.damico@linfaservice.it>
2022-08-18 17:28:55 +0200

    Update isJWT.js

--- src/lib/isJWT.js
+++ src/lib/isJWT.js
@@ -7,15 +7,12 @@ export default function isJWT(str, jwe = false) {
   const dotSplit = str.split('.');
   const len = dotSplit.length;
 
-  /* eslint no-lonely-if: "error" */
   if (!jwe) {
     if (len > 3 || len < 2) {
       return false;
     }
-  } else {
-    if (len < 5 || len > 5) {
-      return false;
-    }
+  } else if (len !== 5) {
+    return false;
   }
 
   return dotSplit.reduce((acc, currElem) => acc && isBase64(currElem, { urlSafe: true }), true);
