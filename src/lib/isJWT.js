06950ccf955967b3c32ce3cdf2da911004c8eeaa
Michele D'Amico <michele.damico@linfaservice.it>
2022-08-18 16:38:17 +0200

    isJWT: adds compatibility for JWE (RFC6516)
    JWT ([RFC7519](https://www.rfc-editor.org/rfc/rfc7519.html) can be:
    - JWS (header.payload.signature) - [RFC7515](https://www.rfc-editor.org/rfc/rfc7515.html)
    - JWE (header.encryption_key.initialization_vector.ciphertext.authentication_tag) - [RFC7516](https://www.rfc-editor.org/rfc/rfc7516.html)
    
    This PR add compatibility to validate a JWT as JWE if thw second parameter is true.

--- src/lib/isJWT.js
+++ src/lib/isJWT.js
@@ -1,14 +1,20 @@
 import assertString from './util/assertString';
 import isBase64 from './isBase64';
 
-export default function isJWT(str) {
+export default function isJWT(str, jwe = false) {
   assertString(str);
 
   const dotSplit = str.split('.');
   const len = dotSplit.length;
 
-  if (len > 3 || len < 2) {
-    return false;
+  if(!jwe) {
+    if (len > 3 || len < 2) {
+      return false;
+    }
+  } else {
+    if (len!=5) {
+      return false; 
+    }
   }
 
   return dotSplit.reduce((acc, currElem) => acc && isBase64(currElem, { urlSafe: true }), true);
