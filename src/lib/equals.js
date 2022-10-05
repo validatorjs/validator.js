03b38803c195bb5572f45213815c6f70413538ba
Tineshnehete <tineshnehete000@gmail.com>
2022-10-05 12:00:53 +0530

    Added Comments For equals

--- src/lib/equals.js
+++ src/lib/equals.js
@@ -1,8 +1,17 @@
 import assertString from './util/assertString';
 
-const deafaultOptions = { sensitivity: undefined, locales: undefined };
+const deafaultOptions = {
+  // set the sensitivity mode ''' base | accent | varient '''
+  sensitivity: undefined,
+  // set local language for ex 'en'
+  locales: undefined,
+};
+
 export default function equals(str, comparison, options = deafaultOptions) {
   assertString(str);
+
+  // If sensitivity option is not defined return basic comparison
+  // else return comaprision with str.localCompare
   return (options.sensitivity === undefined) ?
     (str === comparison) :
     str.localeCompare(comparison, options.locales, { sensitivity: options.sensitivity }) === 0;
