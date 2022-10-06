ac49cf6186a3afc7f3d29eb6c0806328754a6a15
DeepankRx <deepankpushpad10@gmail.com>
2022-10-07 03:37:05 +0530

    whether a string contains emoji or not

---
+++ src/lib/isEmoji.js
@@ -0,0 +1,12 @@
+function isEmoji(str) {
+  var ranges = [
+    '\ud83c[\udf00-\udfff]', // U+1F300 to U+1F3FF
+    '\ud83d[\udc00-\ude4f]', // U+1F400 to U+1F64F
+    '\ud83d[\ude80-\udeff]', // U+1F680 to U+1F6FF
+  ];
+  if (str.match(ranges.join('|'))) {
+    return true;
+  } else {
+    return false;
+  }
+}
