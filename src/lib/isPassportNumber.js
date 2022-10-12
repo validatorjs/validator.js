97ef9610e47174051bc9a6054fd42b9487dbfb9c
Digambar Tupurwadi <digz@kali>
2022-10-12 18:20:10 +0530

    fixed issues prompted by node

--- src/lib/isPassportNumber.js
+++ src/lib/isPassportNumber.js
@@ -48,7 +48,7 @@ const passportRegexByCountryCode = {
   MY: /^[AHK]\d{8}$/, // MALAYSIA
   MX: /^\d{10,11}$/, // MEXICO
   NL: /^[A-Z]{2}[A-Z0-9]{6}\d$/, // NETHERLANDS
-  PHL: /^([A-Z](\d{6}|\d{7}[A-Z]))|([A-Z]{2}(\d{6}|\d{7}))$/, //PHILIPPINES
+  PHL: /^([A-Z](\d{6}|\d{7}[A-Z]))|([A-Z]{2}(\d{6}|\d{7}))$/, // PHILIPPINES
   PL: /^[A-Z]{2}\d{7}$/, // POLAND
   PT: /^[A-Z]\d{6}$/, // PORTUGAL
   RO: /^\d{8,9}$/, // ROMANIA
