afda0bdb168df2572e5d4947fc4a250a9fbb4ad6
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2022-10-18 14:10:14 +0000

    feat(isAfter): allow usage of options object

--- src/lib/isAfter.js
+++ src/lib/isAfter.js
@@ -1,8 +1,22 @@
 import assertString from './util/assertString';
 import toDate from './toDate';
 
-export default function isAfter(str, date = String(new Date())) {
+export default function isAfter(str, options) {
   assertString(str);
+
+  let date;
+
+  if (typeof (options) === 'object') {
+    date = options.date;
+  } else { // backwards compatibility: isAfter(str [, date])
+    // eslint-disable-next-line prefer-rest-params
+    date = arguments[1];
+  }
+
+  if (!date) {
+    date = String(new Date());
+  }
+
   const comparison = toDate(date);
   const original = toDate(str);
   return !!(original && comparison && original > comparison);
--- test/validators.js
+++ test/validators.js
@@ -4993,31 +4993,6 @@ describe('Validators', () => {
     });
   });
 
-  it('should validate dates against a start date', () => {
-    test({
-      validator: 'isAfter',
-      args: ['2011-08-03'],
-      valid: ['2011-08-04', new Date(2011, 8, 10).toString()],
-      invalid: ['2010-07-02', '2011-08-03', new Date(0).toString(), 'foo'],
-    });
-    test({
-      validator: 'isAfter',
-      valid: ['2100-08-04', new Date(Date.now() + 86400000).toString()],
-      invalid: ['2010-07-02', new Date(0).toString()],
-    });
-    test({
-      validator: 'isAfter',
-      args: ['2011-08-03'],
-      valid: ['2015-09-17'],
-      invalid: ['invalid date'],
-    });
-    test({
-      validator: 'isAfter',
-      args: ['invalid date'],
-      invalid: ['invalid date', '2015-09-17'],
-    });
-  });
-
   it('should validate dates against an end date', () => {
     test({
       validator: 'isBefore',
