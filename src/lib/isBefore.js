ae7a8a68d41fca80836fb7f500a5a88f5fd22ae5
Falk Schieber <12937991+pixelbucket-dev@users.noreply.github.com>
2022-10-25 15:48:01 +0200

    Refactor isBefore with options API

--- src/lib/isBefore.js
+++ src/lib/isBefore.js
@@ -1,9 +1,12 @@
 import assertString from './util/assertString';
 import toDate from './toDate';
 
-export default function isBefore(str, date = String(new Date())) {
+export default function isBefore(str, options = {}) {
   assertString(str);
-  const comparison = toDate(date);
+
+  const { comparisonDate = Date().toString() } = options;
+  const comparison = toDate(comparisonDate);
   const original = toDate(str);
+
   return !!(original && comparison && original < comparison);
 }
