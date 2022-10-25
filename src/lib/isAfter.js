9ca431d6bdf661020437af2b8ae3288513018521
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2022-10-25 18:41:32 +0000

    feat(isAfter): allow usage of options object

--- src/lib/isAfter.js
+++ src/lib/isAfter.js
@@ -1,9 +1,11 @@
-import assertString from './util/assertString';
 import toDate from './toDate';
 
-export default function isAfter(str, date = String(new Date())) {
-  assertString(str);
-  const comparison = toDate(date);
-  const original = toDate(str);
-  return !!(original && comparison && original > comparison);
+export default function isAfter(date, options) {
+  // accessing 'arguments' for backwards compatibility: isAfter(str [, date])
+  // eslint-disable-next-line prefer-rest-params
+  const comparisonDate = (typeof options === 'object' ? options.comparisonDate : arguments[1]) || Date().toString();
+
+  const comparison = toDate(comparisonDate);
+  const original = toDate(date);
+  return !!(original > comparison);
 }
