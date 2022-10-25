65a9d7192f0b8a45447300ec3614ec6d81f52266
Falk Schieber <12937991+pixelbucket-dev@users.noreply.github.com>
2022-10-25 19:44:50 +0200

    Fix backwards-compat

--- src/lib/isBefore.js
+++ src/lib/isBefore.js
@@ -1,10 +1,12 @@
 import assertString from './util/assertString';
 import toDate from './toDate';
 
-export default function isBefore(date, options = {}) {
+export default function isBefore(date, options) {
   assertString(date);
 
-  const { comparisonDate = Date().toString() } = options;
+  // accessing 'arguments' for backwards compatibility: isBefore(date [, date])
+  // eslint-disable-next-line prefer-rest-params
+  const comparisonDate = (typeof options === 'object' ? options.comparisonDate : arguments[1]) || Date().toString();
   const comparison = toDate(comparisonDate);
   const original = toDate(date);
 
