17370bdd318b3ee8cd687f568e010de4a5956404
Falk Schieber <12937991+pixelbucket-dev@users.noreply.github.com>
2022-10-25 09:48:05 +0200

    Refactor isAlpha with options API

--- src/lib/isAlpha.js
+++ src/lib/isAlpha.js
@@ -1,23 +1,29 @@
 import assertString from './util/assertString';
 import { alpha } from './alpha';
 
-export default function isAlpha(_str, locale = 'en-US', options = {}) {
-  assertString(_str);
-
-  let str = _str;
-  const { ignore } = options;
-
-  if (ignore) {
-    if (ignore instanceof RegExp) {
-      str = str.replace(ignore, '');
-    } else if (typeof ignore === 'string') {
-      str = str.replace(new RegExp(`[${ignore.replace(/[-[\]{}()*+?.,\\^$|#\\s]/g, '\\$&')}]`, 'g'), ''); // escape regex for ignore
-    } else {
-      throw new Error('ignore should be instance of a String or RegExp');
-    }
+function augmentStringWithIgnoredCharacters(str, ignoredCaharacters) {
+  if (!ignoredCaharacters) {
+    return str;
   }
 
-  if (locale in alpha) {
+  if (ignoredCaharacters instanceof RegExp) {
+    return str.replace(ignoredCaharacters, '');
+  }
+
+  if (typeof ignore === 'string') {
+    return str.replace(new RegExp(`[${ignoredCaharacters.replace(/[-[\]{}()*+?.,\\^$|#\\s]/g, '\\$&')}]`, 'g'), ''); // escape regex for ignore
+  }
+
+  throw new Error('ignore should be instance of a String or RegExp');
+}
+
+export default function isAlpha(_str, options = { locale: 'en-US' }) {
+  assertString(_str);
+
+  const { ignore, locale } = options;
+  const str = augmentStringWithIgnoredCharacters(_str, ignore);
+
+  if (alpha[locale]) {
     return alpha[locale].test(str);
   }
   throw new Error(`Invalid locale '${locale}'`);
