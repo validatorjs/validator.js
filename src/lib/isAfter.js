42a8490fea7c06a41f3793892e918d3affad798c
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2022-10-26 09:50:04 +0000

    refactor: improve comparisonDate

--- src/lib/isAfter.js
+++ src/lib/isAfter.js
@@ -1,9 +1,9 @@
 import toDate from './toDate';
 
 export default function isAfter(date, options) {
-  // accessing 'arguments' for backwards compatibility: isAfter(str [, date])
-  // eslint-disable-next-line prefer-rest-params
-  const comparisonDate = (typeof options === 'object' ? options.comparisonDate : arguments[1]) || Date().toString();
+  // For backwards compatibility:
+  // isAfter(str [, date]), i.e. `options` could be used as argument for the legacy `date`
+  const comparisonDate = options?.comparisonDate || options || Date().toString();
 
   const comparison = toDate(comparisonDate);
   const original = toDate(date);
