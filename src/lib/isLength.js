57a5633b82f1401ce5e32a77d4d549a77df6340e
Vatsal Sanchala <bevatsal1122@gmail.com>
2022-11-12 22:00:29 +0530

    Update src/lib/isLength.js
    Improvement update
    
    Co-authored-by: Falk Schieber <12937991+pixelbucket-dev@users.noreply.github.com>

--- src/lib/isLength.js
+++ src/lib/isLength.js
@@ -20,7 +20,14 @@ export default function isLength(str, options) {
   const surrogatePairs = str.match(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g) || [];
   const len = str.length - presentationSequences.length - surrogatePairs.length;
   result = len >= min && (typeof max === 'undefined' || len <= max);
-  if (result === false || typeof exact === 'undefined') return result;
+const isInsideRange =
+len >= min && (typeof max === 'undefined' || len <= max);
+
+if (isInsideRange && Array.isArray(options?.discreteLengths)) {
+    return options.discreteLengths.some(discreteLen => discreteLen === len);
+}
+
+return isInsideRange;
   if (Array.isArray(exact) === true) {
     for (let oneElem of exact) {
       if (len === oneElem) {
