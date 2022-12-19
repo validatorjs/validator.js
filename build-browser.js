db542dd16d8c522557e2d38ed62bea7aecaf8748
Panagiotis Papadopoulos <pano_90@gmx.net>
2022-12-19 01:58:03 +0100

    build(build-browser): update rollup options
    * options have been renamed in rollup
    * babelHelpers: 'bundled' added explicitly to avoid rollup nagging about it not being set explicitly

--- build-browser.js
+++ build-browser.js
@@ -1,24 +1,25 @@
 /* eslint import/no-extraneous-dependencies: 0 */
 import fs from "fs";
 import { rollup } from "rollup";
-import babel from "rollup-plugin-babel";
+import babel from "@rollup/plugin-babel";
 import babelPresetEnv from "@babel/preset-env";
 import pkg from "./package.json";
 
 rollup({
-  entry: "src/index.js",
+  input: "src/index.js",
   plugins: [
     babel({
       presets: [[babelPresetEnv, { modules: false }]],
       babelrc: false,
+      babelHelpers: "bundled",
     }),
   ],
 })
   .then((bundle) =>
     bundle.write({
-      dest: "validator.js",
+      file: "validator.js",
       format: "umd",
-      moduleName: pkg.name,
+      name: pkg.name,
       banner: `/*!\n${String(fs.readFileSync("./LICENSE"))
         .trim()
         .split("\n")
