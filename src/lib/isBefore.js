de17c53da033955a3b5a6f2b67ffb5d6fa2c2afe
-l <12937991+pixelbucket-dev@users.noreply.github.com>
2023-01-03 21:28:09 +0100

    Add line break according to code review

--- src/lib/isBefore.js
+++ src/lib/isBefore.js
@@ -4,6 +4,7 @@ export default function isBefore(str, options) {
   // accessing 'arguments' for backwards compatibility: isBefore(str [, comparisonDate])
   // eslint-disable-next-line prefer-rest-params
   const comparisonDate = (typeof options === 'object' ? options.comparisonDate : arguments[1]) || Date().toString();
+  
   const comparison = toDate(comparisonDate);
   const original = toDate(str);
 
