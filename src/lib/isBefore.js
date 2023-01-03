44eda79825ac0d504f2f2190c6b20e97c1cac706
-l <12937991+pixelbucket-dev@users.noreply.github.com>
2023-01-03 21:26:23 +0100

    Change arg name according to code review

--- src/lib/isBefore.js
+++ src/lib/isBefore.js
@@ -1,11 +1,11 @@
 import toDate from './toDate';
 
-export default function isBefore(date, options) {
-  // accessing 'arguments' for backwards compatibility: isBefore(date [, comparisonDate])
+export default function isBefore(str, options) {
+  // accessing 'arguments' for backwards compatibility: isBefore(str [, comparisonDate])
   // eslint-disable-next-line prefer-rest-params
   const comparisonDate = (typeof options === 'object' ? options.comparisonDate : arguments[1]) || Date().toString();
   const comparison = toDate(comparisonDate);
-  const original = toDate(date);
+  const original = toDate(str);
 
   return !!(original < comparison);
 }
