a9983becc09cf8d729fff4e1707952b33e99b8bc
Songyue Wang <me@songyue.wang>
2023-01-05 03:03:39 +0800

    Added support for zh-HK in isIdentityCard

--- src/lib/isIdentityCard.js
+++ src/lib/isIdentityCard.js
@@ -346,29 +346,31 @@ const validators = {
     // sanitize user input
     str = str.trim();
 
-    // HKID number starts with a letter, followed by 6 digits,
+    // HKID number starts with 1 or 2 letters, followed by 6 digits,
     // then a checksum contained in square / round brackets or nothing
-    const regexHKID = new RegExp('^[A-Z]([0-9]{6})((\\([0-9A]\\))|(\\[[0-9A]\\])|([0-9A]))$', 'gm');
+    const regexHKID = new RegExp('^[A-Z]{1,2}[0-9]{6}(\\([0-9A]\\))|(\\[[0-9A]\\])|([0-9A])$', 'gm');
+    const regexIsDigit = new RegExp('^[0-9]$');
 
     // convert the user input to all uppercase and apply regex
     str = str.toUpperCase();
     if (!regexHKID.exec(str)) return false;
-
-    // remove any brackets
     str = str.replace(/\[|\]|\(|\)/g, '');
+    const strArr = str.split('');
 
-    const letterVal = str.charCodeAt(0) - 64;
-    let checkSumVal = letterVal * 8;
-    for (let i = 1; i <= 6; i++) {
-      checkSumVal += (str[i] * (8 - i));
+    if (strArr.length === 8) strArr.unshift('3');
+    let checkSumVal = 0;
+    for (let i = 0; i <= 7; i++) {
+      let convertedChar;
+      if (!regexIsDigit.exec(strArr[i])) convertedChar = (strArr[i].charCodeAt(0) - 55) % 11;
+      else convertedChar = strArr[i];
+      checkSumVal += (convertedChar * (9 - i));
     }
     checkSumVal %= 11;
     let checkSumConverted;
     if (checkSumVal === 0) checkSumConverted = '0';
     else if (checkSumVal === 1) checkSumConverted = 'A';
     else checkSumConverted = String(11 - checkSumVal);
-
-    if (checkSumConverted === str[7]) return true;
+    if (checkSumConverted === strArr.at(-1)) return true;
     return false;
   },
   'zh-TW': (str) => {
