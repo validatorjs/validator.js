d51d218a5980378b9c8048347a4a3f3ca4ba51e9
Richard <95219569+richard-bridgeman@users.noreply.github.com>
2023-01-11 13:36:04 +0000

    Update isStrongPassword.js

--- src/lib/isStrongPassword.js
+++ src/lib/isStrongPassword.js
@@ -4,7 +4,7 @@ import assertString from './util/assertString';
 const upperCaseRegex = /^[A-Z]$/;
 const lowerCaseRegex = /^[a-z]$/;
 const numberRegex = /^[0-9]$/;
-const symbolRegex = /^[-#!$@%^&*()_+|~=`{}\[\]:";'<>?,.\/ ]$/;
+const symbolRegex = /^[-#!$@%^&*()_+|~=`{}\[\]:";'<>?,.\/ ]$£/;
 
 const defaultOptions = {
   minLength: 8,
