39b880de2d8a7c81dfa805a9dd9857fb72d846e5
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2023-01-30 20:18:26 +0000

    test(isHexadecimal): split isHexadecimal tests

--- test/validators.test.js
+++ test/validators.test.js
@@ -3752,33 +3752,6 @@ describe('Validators', () => {
     });
   });
 
-  it('should validate hexadecimal strings', () => {
-    test({
-      validator: 'isHexadecimal',
-      valid: [
-        'deadBEEF',
-        'ff0044',
-        '0xff0044',
-        '0XfF0044',
-        '0x0123456789abcDEF',
-        '0X0123456789abcDEF',
-        '0hfedCBA9876543210',
-        '0HfedCBA9876543210',
-        '0123456789abcDEF',
-      ],
-      invalid: [
-        'abcdefg',
-        '',
-        '..',
-        '0xa2h',
-        '0xa20x',
-        '0x0123456789abcDEFq',
-        '0hfedCBA9876543210q',
-        '01234q56789abcDEF',
-      ],
-    });
-  });
-
   it('should validate octal strings', () => {
     test({
       validator: 'isOctal',
