c7978166c78a1e5f0c430a03c032d01507613355
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2023-01-30 20:23:34 +0000

    test(isHexColor): split isHexColor tests

--- test/validators.test.js
+++ test/validators.test.js
@@ -3752,25 +3752,6 @@ describe('Validators', () => {
     });
   });
 
-  it('should validate hexadecimal color strings', () => {
-    test({
-      validator: 'isHexColor',
-      valid: [
-        '#ff0000ff',
-        '#ff0034',
-        '#CCCCCC',
-        '0f38',
-        'fff',
-        '#f00',
-      ],
-      invalid: [
-        '#ff',
-        'fff0a',
-        '#ff12FG',
-      ],
-    });
-  });
-
   it('should validate HSL color strings', () => {
     test({
       validator: 'isHSL',
