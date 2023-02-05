18d652bac4347da5d6f8a2b9042c6ce29d917ad8
pixelbucket.dev <12937991+pixelbucket-dev@users.noreply.github.com>
2023-02-05 21:37:33 +0100

    Make isAfter arguments more robust

--- src/lib/isAfter.js
+++ src/lib/isAfter.js
@@ -3,7 +3,7 @@ import toDate from './toDate';
 export default function isAfter(date, options) {
   // For backwards compatibility:
   // isAfter(str [, date]), i.e. `options` could be used as argument for the legacy `date`
-  const comparisonDate = options?.comparisonDate || options || Date().toString();
+  const comparisonDate = (typeof options === 'object' ? options.comparisonDate : options) || Date().toString();
 
   const comparison = toDate(comparisonDate);
   const original = toDate(date);
