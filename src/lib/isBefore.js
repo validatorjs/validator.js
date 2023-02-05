05d07eab5d92bb3df0e7315269b6eff747e8051d
pixelbucket.dev <12937991+pixelbucket-dev@users.noreply.github.com>
2023-02-05 21:26:59 +0100

    Remove arguments: linter error

--- src/lib/isBefore.js
+++ src/lib/isBefore.js
@@ -3,7 +3,7 @@ import toDate from './toDate';
 export default function isBefore(str, options) {
   // accessing 'arguments' for backwards compatibility: isBefore(str [, comparisonDate])
   // eslint-disable-next-line prefer-rest-params
-  const comparisonDate = (typeof options === 'object' ? options.comparisonDate : arguments[1]) || Date().toString();
+  const comparisonDate = (typeof options === 'object' ? options.comparisonDate : options) || Date().toString();
 
   const comparison = toDate(comparisonDate);
   const original = toDate(str);
