57d89454709e47bf1919a444a91848b0f0e31ac3
pixelbucket.dev <12937991+pixelbucket-dev@users.noreply.github.com>
2023-02-06 21:41:54 +0100

    Split test cases into given and default end date

--- test/validators/isBefore.test.js
+++ test/validators/isBefore.test.js
@@ -1,58 +1,35 @@
+import { describe } from 'mocha';
 import test from '../testFunctions';
 
 describe('isBefore', () => {
-  it('should validate dates against an end date', () => {
-    test({
-      validator: 'isBefore',
-      args: [{ comparisonDate: '08/04/2011' }],
-      valid: ['2010-07-02', '2010-08-04', new Date(0).toString()],
-      invalid: ['08/04/2011', new Date(2011, 9, 10).toString()],
+  describe('should validate dates a given end date', () => {
+    describe('new syntax', () => {
+      test({
+        validator: 'isBefore',
+        args: [{ comparisonDate: '08/04/2011' }],
+        valid: ['2010-07-02', '2010-08-04', new Date(0).toString()],
+        invalid: ['08/04/2011', new Date(2011, 9, 10).toString()],
+      });
+      test({
+        validator: 'isBefore',
+        args: [{ comparisonDate: new Date(2011, 7, 4).toString() }],
+        valid: ['2010-07-02', '2010-08-04', new Date(0).toString()],
+        invalid: ['08/04/2011', new Date(2011, 9, 10).toString()],
+      });
+      test({
+        validator: 'isBefore',
+        args: [{ comparisonDate: '2011-08-03' }],
+        valid: ['1999-12-31'],
+        invalid: ['invalid date'],
+      });
+      test({
+        validator: 'isBefore',
+        args: [{ comparisonDate: 'invalid date' }],
+        invalid: ['invalid date', '1999-12-31'],
+      });
     });
-    test({
-      validator: 'isBefore',
-      args: [{ comparisonDate: new Date(2011, 7, 4).toString() }],
-      valid: ['2010-07-02', '2010-08-04', new Date(0).toString()],
-      invalid: ['08/04/2011', new Date(2011, 9, 10).toString()],
-    });
-    test({
-      validator: 'isBefore',
-      valid: [
-        '2000-08-04',
-        new Date(0).toString(),
-        new Date(Date.now() - 86400000).toString(),
-      ],
-      invalid: ['2100-07-02', new Date(2217, 10, 10).toString()],
-    });
-    test({
-      validator: 'isBefore',
-      args: [{ comparisonDate: '2011-08-03' }],
-      valid: ['1999-12-31'],
-      invalid: ['invalid date'],
-    });
-    test({
-      validator: 'isBefore',
-      args: [{ comparisonDate: 'invalid date' }],
-      invalid: ['invalid date', '1999-12-31'],
-    });
-    test({
-      validator: 'isBefore',
-      args: [], // will fall back to the current date
-      valid: ['1999-06-07'],
-    });
-    test({
-      validator: 'isBefore',
-      args: [undefined], // will fall back to the current date
-      valid: ['1999-06-07'],
-    });
-    test({
-      validator: 'isBefore',
-      args: [{ comparisonDate: undefined }], // will fall back to the current date
-      valid: ['1999-06-07'],
-    });
-  });
 
-  describe('(legacy syntax)', () => {
-    it('should validate dates against an end date', () => {
+    describe('legacy syntax', () => {
       test({
         validator: 'isBefore',
         args: ['08/04/2011'],
@@ -67,6 +44,22 @@ describe('isBefore', () => {
       });
       test({
         validator: 'isBefore',
+        args: ['2011-08-03'],
+        valid: ['1999-12-31'],
+        invalid: ['invalid date'],
+      });
+      test({
+        validator: 'isBefore',
+        args: ['invalid date'],
+        invalid: ['invalid date', '1999-12-31'],
+      });
+    });
+  });
+
+  describe('should validate dates a default end date', () => {
+    describe('new syntax', () => {
+      test({
+        validator: 'isBefore',
         valid: [
           '2000-08-04',
           new Date(0).toString(),
@@ -76,14 +69,50 @@ describe('isBefore', () => {
       });
       test({
         validator: 'isBefore',
-        args: ['2011-08-03'],
-        valid: ['1999-12-31'],
-        invalid: ['invalid date'],
+        args: undefined, // will fall back to the current date
+        valid: ['1999-06-07'],
       });
       test({
         validator: 'isBefore',
-        args: ['invalid date'],
-        invalid: ['invalid date', '1999-12-31'],
+        args: [], // will fall back to the current date
+        valid: ['1999-06-07'],
+      });
+      test({
+        validator: 'isBefore',
+        args: [undefined], // will fall back to the current date
+        valid: ['1999-06-07'],
+      });
+      test({
+        validator: 'isBefore',
+        args: [{ comparisonDate: undefined }], // will fall back to the current date
+        valid: ['1999-06-07'],
+      });
+    });
+
+    describe('legacy syntax', () => {
+      test({
+        validator: 'isBefore',
+        valid: [
+          '2000-08-04',
+          new Date(0).toString(),
+          new Date(Date.now() - 86400000).toString(),
+        ],
+        invalid: ['2100-07-02', new Date(2217, 10, 10).toString()],
+      });
+      test({
+        validator: 'isBefore',
+        args: undefined, // will fall back to the current date
+        valid: ['1999-06-07'],
+      });
+      test({
+        validator: 'isBefore',
+        args: [], // will fall back to the current date
+        valid: ['1999-06-07'],
+      });
+      test({
+        validator: 'isBefore',
+        args: [undefined], // will fall back to the current date
+        valid: ['1999-06-07'],
       });
     });
   });
