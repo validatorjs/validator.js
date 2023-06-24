bcbd56a14e11fa72fe355af5b5c4bbbce2ddae13
Kishore Renangi <kishorer@knownuggets.com>
2023-06-24 13:41:31 +0530

    fix(test): isMobilePhone

--- test/isMobilePhone.js
+++ test/isMobilePhone.js
@@ -4,8 +4,8 @@ import validator from '../validator';
 describe('test isMobilePhone', () => {
   describe('test getMobileLocalPattern', () => {
     it('should test the Thailand number', () => {
-      assert.equal(validator.getMobileLocalPattern('th-TH').test('+6624915095'), false);
-      assert.equal(validator.getMobileLocalPattern('th-TH').test('+66892001987'), true);
+      assert.equal(validator.isMobilePhone('+6624915095', 'th-TH'), false);
+      assert.equal(validator.isMobilePhone('+66892001987', 'th-TH'), true);
     });
   });
 });
