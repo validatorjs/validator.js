3507d272b09e67e8762397450f8b84378ea64bb1
Prathamesh Lakhapati <114794439+Prathamesh061@users.noreply.github.com>
2023-06-26 09:17:58 +0530

    fix(isJWT): fix validation issue in isJWT function (#2217)
    * Update isJWT.js
    
    fix: Ensure isJWT returns false for 2 part invalid JWT tokens
    
    Previously, the isJWT function would return true for 2 part invalid JWT tokens. This has been fixed by updating the isJWT function to return false for such tokens.
    
    * Update validators.test.js
    
    Added test case for validating JSON web tokens (JWT)
    
    * Update validators.test.js
    
    Removed trailing spaces
    
    * Update validators.test.js
    
    Refactor tests in isjwt and remove redundant test case
    
    * Update validators.test.js
    
    Removed redundant test in isJWT

--- src/lib/isJWT.js
+++ src/lib/isJWT.js
@@ -7,7 +7,7 @@ export default function isJWT(str) {
   const dotSplit = str.split('.');
   const len = dotSplit.length;
 
-  if (len > 3 || len < 2) {
+  if (len !== 3) {
     return false;
   }
 
--- test/validators.test.js
+++ test/validators.test.js
@@ -4720,10 +4720,11 @@ describe('Validators', () => {
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb3JlbSI6Imlwc3VtIn0.ymiJSsMJXR6tMSr8G9usjQ15_8hKPDv_CArLhxw28MI',
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkb2xvciI6InNpdCIsImFtZXQiOlsibG9yZW0iLCJpcHN1bSJdfQ.rRpe04zbWbbJjwM43VnHzAboDzszJtGrNsUxaqQ-GQ8',
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqb2huIjp7ImFnZSI6MjUsImhlaWdodCI6MTg1fSwiamFrZSI6eyJhZ2UiOjMwLCJoZWlnaHQiOjI3MH19.YRLPARDmhGMC3BBk_OhtwwK21PIkVCqQe8ncIRPKo-E',
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ', // No signature
       ],
       invalid: [
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NSIsIm5hbWUiOiJKb2huIERvZSIsImlhdCI6MTUxNjIzOTAyMn0',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NSIsIm5hbWUiOiJKb2huIERvZSIsImlhdCI6MTYxNjY1Mzg3Mn0.eyJpc3MiOiJodHRwczovL2V4YW1wbGUuY29tIiwiaWF0IjoxNjE2NjUzODcyLCJleHAiOjE2MTY2NTM4ODJ9.a1jLRQkO5TV5y5ERcaPAiM9Xm2gBdRjKrrCpHkGr_8M',
         '$Zs.ewu.su84',
         'ks64$S/9.dy$§kz.3sd73b',
       ],
