55a6a4146f55464953be8d2dd34103cb1bdf743c
davep80 <davidpaq1@gmail.com>
2023-08-13 14:44:09 -0400

    regex refactor

--- src/lib/isCSV.js
+++ src/lib/isCSV.js
@@ -2,6 +2,6 @@ import assertString from './util/assertString';
 
 export default function isCSV(str) {
     assertString(str);
-    const regex = /^(\s*[\w\d\s-]+\s*,\s*)*[\w\d\s-]+\s*$/;
+    const regex = /^(?:\s*[\w\d\s-]+(?:\s*,\s*|$))+$/;
     return regex.test(str);
 }
