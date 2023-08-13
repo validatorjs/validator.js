d337931d86da6edf16bd680469d163d6c9a28718
davep80 <davidpaq1@gmail.com>
2023-08-13 14:36:35 -0400

    new line formatting

--- src/lib/isCSV.js
+++ src/lib/isCSV.js
@@ -1,7 +1,7 @@
 import assertString from './util/assertString';
 
 export default function isCSV(str) {
-  assertString(str);
-  const regex = /^(\s*[\w\d\s-]+\s*,\s*)*[\w\d\s-]+\s*$/;
-  return regex.test(str);
+    assertString(str);
+    const regex = /^(\s*[\w\d\s-]+\s*,\s*)*[\w\d\s-]+\s*$/;
+    return regex.test(str);
 }
