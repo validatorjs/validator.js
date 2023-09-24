d754097408d9fbd4c9f06f7fd2cc339630f94f6d
Rahul Nikam <codewithrahulnikam@gmail.com>
2023-09-24 13:56:36 +0530

    feat: added validation for Pancard and lint error solved

--- src/lib/isPancard.js
+++ src/lib/isPancard.js
@@ -1,5 +1,11 @@
-import assertString from "./util/assertString";
+import assertString from './util/assertString';
 
+/**
+ * Check if str is a valid pan card number
+ *
+ * @param {string} str
+ * @return {boolean}
+ */
 export default function isPancard(str) {
   assertString(str);
   const pancardRegex = /^[A-Z]{5}[0-9]{4}[A-Z]$/;
