a34fa083ff3afb08fbacbad6cf48f2d0bcb5b89c
Rahul Nikam <codewithrahulnikam@gmail.com>
2023-09-26 19:40:15 +0530

    feat(isUPC): UPC is Universal Product Code widely used worldwide for tracking trade items

--- src/lib/isUPC.js
+++ src/lib/isUPC.js
@@ -1,8 +1,8 @@
 import assertString from './util/assertString';
 
-const upcRegex = /^\d{12}$/;
 
 export default function isPUC(str) {
   assertString(str);
+  const upcRegex = /^\d{12}$/;
   return upcRegex.test(str);
 }
