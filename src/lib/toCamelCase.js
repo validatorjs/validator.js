f6cc3a4ecccfe23b0d4cc90c37ebca8714180d7d
Muhammad Haris <muhammad.haris1@outlook.com>
2023-10-15 14:45:59 +0200

    optimized code

--- src/lib/toCamelCase.js
+++ src/lib/toCamelCase.js
@@ -3,14 +3,11 @@ import assertString from "./util/assertString";
 export default function toCamelCase(inputString) {
   assertString(inputString);
 
-  // Split the input string into words using spaces, hyphens, or underscores as separators
   const words = inputString.split(/[\s\-_]+/);
 
-  // Capitalize the first letter of each word except the first one
-  for (let i = 1; i < words.length; i++) {
-    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
-  }
+  words.map((word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  });
 
-  // Join the words back together to form the camelCase string
-  return words.join('');
+  return words.join("");
 }
