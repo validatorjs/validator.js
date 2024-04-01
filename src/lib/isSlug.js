6db8b3bdf935a64016b3f5d16f9ae7431be2d217
alinaghale88 <alinaghale.official@gmail.com>
2024-04-01 01:38:03 -0700

    Update regex in isSlug.js to match all test cases

--- src/lib/isSlug.js
+++ src/lib/isSlug.js
@@ -1,6 +1,7 @@
 import assertString from './util/assertString';
 
-let charsetRegex = /^(?!.*\.)[^\s-_][a-z0-9-\\][^\s]*[^-_\s]$/;
+let charsetRegex = /^(?![-_])^[^\s](?!.*?[-_]{2,})[a-z0-9*&\\]*(?:[-_][a-z0-9*&\\]+)*[^-_\s](?!\.)$/;
+// /^(?!.*\.)[^\s-_][a-z0-9-\\][^\s]*[^-_\s]$/
 
 export default function isSlug(str) {
   assertString(str);
