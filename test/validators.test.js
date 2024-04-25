19f11cf77685652d15ed516b1ecdaf477a5ee89b
Songyue Wang <98693645+songyuew@users.noreply.github.com>
2024-04-25 18:04:32 +0800

    feat: added isAbaRouting validator (#2143)
    Added isAbaRouting validator, new validator. 
    Check whether a string is ABA routing number for US bank account & cheque
    
    ref: https://en.wikipedia.org/wiki/ABA_routing_transit_number

--- test/validators.test.js
+++ test/validators.test.js
@@ -5298,6 +5298,26 @@ describe('Validators', () => {
     });
   });
 
+  it('should validate ABA routing number', () => {
+    test({
+      validator: 'isAbaRouting',
+      valid: [
+        '322070381',
+        '011103093',
+        '263170175',
+        '124303065',
+      ],
+      invalid: [
+        '426317017',
+        '789456124',
+        '603558459',
+        'qwerty',
+        '12430306',
+        '382070381',
+      ],
+    });
+  });
+
   it('should validate IBAN', () => {
     test({
       validator: 'isIBAN',
