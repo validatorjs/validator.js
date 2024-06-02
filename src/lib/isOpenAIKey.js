94fad7988f1eb5b94abc8550b088c67e6df94d32
Daniyal Qureshi <daniyalqureshi268@gmail.com>
2024-06-02 05:59:20 -0700

    fix: removed extra empty line

--- src/lib/isOpenAIKey.js
+++ src/lib/isOpenAIKey.js
@@ -2,7 +2,6 @@ import assertString from './util/assertString';
 
 const openAIKey = /^sk-[A-Za-z0-9]{48}$/;
 
-
 export default function isOpenAIKey(str) {
   assertString(str);
   return openAIKey.test(str);
