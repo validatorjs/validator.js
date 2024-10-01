639a22b70e4f19442b535db26bc3fbc8d919cbcc
Vivek Halder <139625193+VivekHalder@users.noreply.github.com>
2024-10-01 12:00:07 +0530

    Revert "feat: Add scientific notation support to isNumeric validator"
    This reverts commit f888607fe4dd489fc34efcb168f32cae16b2a0c4.

--- src/lib/isNumeric.js
+++ src/lib/isNumeric.js
@@ -3,17 +3,10 @@ import { decimal } from './alpha';
 
 const numericNoSymbols = /^[0-9]+$/;
 
-export default function isNumeric(str, options = { no_symbols: false }) {
-  assertString(str); // verify if the str is a string, if not report a TypeError
-
-  // destructure options to extract the required properties
-  const { locale, no_symbols } = options;
-
-  // deciding the separator upfront (default separator is '.')
-  const decimalSeparator = locale ? decimal[locale] : '.';
-
-  // setting the regex depending on the value of no_symbols
-  const regex = no_symbols ? numericNoSymbols : `^[+-]?([0-9]*[${ decimalSeparator }])?[0-9]+([eE][+-]?[0-9]+)?$`;
-
-  return regex.test(str);
+export default function isNumeric(str, options) {
+  assertString(str);
+  if (options && options.no_symbols) {
+    return numericNoSymbols.test(str);
+  }
+  return (new RegExp(`^[+-]?([0-9]*[${(options || {}).locale ? decimal[options.locale] : '.'}])?[0-9]+$`)).test(str);
 }
