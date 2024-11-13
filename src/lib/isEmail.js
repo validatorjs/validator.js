3842d1afec43c740ba34bd0f196364bc97aa61c1
Wei Kang <weikangchia@gmail.com>
2024-11-13 17:13:17 +0800

    fix code logic for checkHost

--- src/lib/isEmail.js
+++ src/lib/isEmail.js
@@ -97,12 +97,12 @@ export default function isEmail(str, options) {
   const domain = parts.pop();
   const lower_domain = domain.toLowerCase();
 
-  if (options.host_blacklist.length > 0) {
-    return !checkHost(lower_domain, options.host_blacklist);
+  if (options.host_blacklist.length > 0 && checkHost(lower_domain, options.host_blacklist)) {
+    return false;
   }
 
-  if (options.host_whitelist.length > 0) {
-    return checkHost(lower_domain, options.host_whitelist);
+  if (options.host_whitelist.length > 0 && !checkHost(lower_domain, options.host_whitelist)) {
+    return false;
   }
 
   let user = parts.join('@');
