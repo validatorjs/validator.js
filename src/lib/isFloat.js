b0b3e72e95a6f704f625a9a45748314baa502f6c
francesco <francesco.bagnoli.69@gmail.com>
2025-01-07 10:48:44 +0100

    perf: avoid useless regex

--- src/lib/isFloat.js
+++ src/lib/isFloat.js
@@ -2,15 +2,19 @@ import assertString from './util/assertString';
 import isNullOrUndefined from './util/nullUndefinedCheck';
 import { decimal } from './alpha';
 
+const regExIsFloat = (str, options) => {
+  const float = new RegExp(`^(?:[-+])?(?:[0-9]+)?(?:\\${options.locale ? decimal[options.locale] : '.'}[0-9]*)?(?:[eE][\\+\\-]?(?:[0-9]+))?$`);
+  return float.test(str);
+}
+
 export default function isFloat(str, options) {
   assertString(str);
   options = options || {};
-  const float = new RegExp(`^(?:[-+])?(?:[0-9]+)?(?:\\${options.locale ? decimal[options.locale] : '.'}[0-9]*)?(?:[eE][\\+\\-]?(?:[0-9]+))?$`);
   if (str === '' || str === '.' || str === ',' || str === '-' || str === '+') {
     return false;
   }
   const value = parseFloat(str.replace(',', '.'));
-  return float.test(str) &&
+  return (options.locale ? regExIsFloat(str) : (!isNaN(value) || regExIsFloat(str)) ) &&
     (!options.hasOwnProperty('min') || isNullOrUndefined(options.min) || value >= options.min) &&
     (!options.hasOwnProperty('max') || isNullOrUndefined(options.max) || value <= options.max) &&
     (!options.hasOwnProperty('lt') || isNullOrUndefined(options.lt) || value < options.lt) &&
