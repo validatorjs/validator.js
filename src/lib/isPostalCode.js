da319983310f8513136663cb2a424e70fe21cce2
Crocsx <Crocsx.gaming@gmail.com>
2025-02-10 13:09:17 +0900

    feat(isPostalCode): Allow taiwan 3+3 format

--- src/lib/isPostalCode.js
+++ src/lib/isPostalCode.js
@@ -69,7 +69,7 @@ const patterns = {
   SK: /^\d{3}\s?\d{2}$/,
   TH: fiveDigit,
   TN: fourDigit,
-  TW: /^\d{3}(\d{2})?$/,
+  TW: /^\d{3}(\d{2,3})?$/,
   UA: fiveDigit,
   US: /^\d{5}(-\d{4})?$/,
   ZA: fourDigit,
