122bf984e0b34ce8c8302796e28801112b519ac8
Numbers0689 <firewatchsai@gmail.com>
2025-03-20 17:55:51 +0530

    Add testcase with default option for ishexcolor

--- test/validators.test.js
+++ test/validators.test.js
@@ -4717,6 +4717,25 @@ describe('Validators', () => {
     });
     test({
       validator: 'isHexColor',
+      args: [{ require_hashtag: false }],
+      valid: [
+        '#ff0000ff',
+        '#ff0034',
+        '#CCCCCC',
+        '0f38',
+        'fff',
+        '#f00',
+      ],
+      invalid: [
+        '#ff',
+        'fff0a',
+        '#ff12FG',
+        '#######',
+        '',
+      ],
+    });
+    test({
+      validator: 'isHexColor',
       args: [{ require_hashtag: true }],
       valid: [
         '#ff0000ff',
