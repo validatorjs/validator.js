650a2fabfb51154f5a17e5b5a791b04c2312d4a9
Falk Schieber <12937991+pixelbucket-dev@users.noreply.github.com>
2025-03-27 14:03:28 +0000

    feat(isBefore): allow usage of options object (#2088)
    * refactor: allow for splitting tests to different files
    
    * feat(isAfter): allow usage of options object
    
    * style: make options italic
    
    * refactor: rename test file extension to .test.js
    
    * refactor: rename test-functions to testFunctions
    
    * refactor: implement suggestion from #2019 review
    
    * refactor: remove custom repeat to use native function
    
    * refactor: implement suggestion new Date
    
    * Refactor isBefore with options API
    
    * Refactor isBefore tests
    
    * Refactor to simplify logic
    
    * Update README
    
    * Refactor logic
    
    * Improve README formatting
    
    * Fix backwards-compat
    
    * Remove redundant string assertion
    
    * Fix comment
    
    * Reinstate legacy tests
    
    * Change arg name according to code review
    
    * Add line break according to code review
    
    * Revert change of simplifying toDate
    
    * Fix whitespace issues
    
    * Fix test
    
    * Fix tests
    
    * Format file for consistency with isBefore
    
    * Remove redundant file
    
    * Remove old tests
    
    * Add tests for undefined args
    
    * Remove arguments: linter error
    
    * Improve comment
    
    * Use recommended variable name
    
    * Improve readme text according to code review
    
    * Make isAfter arguments more robust
    
    * Split test cases into given and default end date
    
    ---------
    
    Co-authored-by: Rik Smale <13023439+WikiRik@users.noreply.github.com>

--- test/validators.test.js
+++ test/validators.test.js
@@ -5650,41 +5650,6 @@ describe('Validators', () => {
     });
   });
 
-  it('should validate dates against an end date', () => {
-    test({
-      validator: 'isBefore',
-      args: ['08/04/2011'],
-      valid: ['2010-07-02', '2010-08-04', new Date(0).toString()],
-      invalid: ['08/04/2011', new Date(2011, 9, 10).toString()],
-    });
-    test({
-      validator: 'isBefore',
-      args: [new Date(2011, 7, 4).toString()],
-      valid: ['2010-07-02', '2010-08-04', new Date(0).toString()],
-      invalid: ['08/04/2011', new Date(2011, 9, 10).toString()],
-    });
-    test({
-      validator: 'isBefore',
-      valid: [
-        '2000-08-04',
-        new Date(0).toString(),
-        new Date(Date.now() - 86400000).toString(),
-      ],
-      invalid: ['2100-07-02', new Date(2217, 10, 10).toString()],
-    });
-    test({
-      validator: 'isBefore',
-      args: ['2011-08-03'],
-      valid: ['1999-12-31'],
-      invalid: ['invalid date'],
-    });
-    test({
-      validator: 'isBefore',
-      args: ['invalid date'],
-      invalid: ['invalid date', '1999-12-31'],
-    });
-  });
-
   it('should validate ABA routing number', () => {
     test({
       validator: 'isAbaRouting',
