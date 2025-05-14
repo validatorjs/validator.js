165894e2a89831a8437e4274a5df0f2a39ff2497
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2025-05-14 15:48:24 +0000

    maintenance: 2505 release

--- src/index.js
+++ src/index.js
@@ -130,7 +130,7 @@ import isStrongPassword from './lib/isStrongPassword';
 
 import isVAT from './lib/isVAT';
 
-const version = '13.15.0';
+const version = '13.15.1';
 
 const validator = {
   version,
