0568fb7e3a46d030bbca993bffaa55fa63bcd9cc
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2025-11-05 18:06:44 +0100

    Test(isLength): Relocate length validation tests

--- test/validators/isLength.test.js
+++ test/validators/isLength.test.js
@@ -42,4 +42,103 @@ describe('isLength', () => {
       valid: ['test\uFE0F\uFE0F\uFE0F\uFE0F\uFE0F\uFE0F'],
     });
   });
+
+  it('should validate strings by length (deprecated api)', () => {
+    test({
+      validator: 'isLength',
+      args: [2],
+      valid: ['abc', 'de', 'abcd'],
+      invalid: ['', 'a'],
+    });
+    test({
+      validator: 'isLength',
+      args: [2, 3],
+      valid: ['abc', 'de'],
+      invalid: ['', 'a', 'abcd'],
+    });
+    test({
+      validator: 'isLength',
+      args: [2, 3],
+      valid: ['干𩸽', '𠮷野家'],
+      invalid: ['', '𠀋', '千竈通り'],
+    });
+    test({
+      validator: 'isLength',
+      args: [0, 0],
+      valid: [''],
+      invalid: ['a', 'ab'],
+    });
+  });
+
+  it('should validate strings by length', () => {
+    test({
+      validator: 'isLength',
+      args: [{ min: 2 }],
+      valid: ['abc', 'de', 'abcd'],
+      invalid: ['', 'a'],
+    });
+    test({
+      validator: 'isLength',
+      args: [{ min: 2, max: 3 }],
+      valid: ['abc', 'de'],
+      invalid: ['', 'a', 'abcd'],
+    });
+    test({
+      validator: 'isLength',
+      args: [{ min: 2, max: 3 }],
+      valid: ['干𩸽', '𠮷野家'],
+      invalid: ['', '𠀋', '千竈通り'],
+    });
+    test({
+      validator: 'isLength',
+      args: [{ max: 3 }],
+      valid: ['abc', 'de', 'a', ''],
+      invalid: ['abcd'],
+    });
+    test({
+      validator: 'isLength',
+      args: [{ max: 6, discreteLengths: 5 }],
+      valid: ['abcd', 'vfd', 'ff', '', 'k'],
+      invalid: ['abcdefgh', 'hfjdksks'],
+    });
+    test({
+      validator: 'isLength',
+      args: [{ min: 2, max: 6, discreteLengths: 5 }],
+      valid: ['bsa', 'vfvd', 'ff'],
+      invalid: ['', ' ', 'hfskdunvc'],
+    });
+    test({
+      validator: 'isLength',
+      args: [{ min: 1, discreteLengths: 2 }],
+      valid: [' ', 'hello', 'bsa'],
+      invalid: [''],
+    });
+    test({
+      validator: 'isLength',
+      args: [{ max: 0 }],
+      valid: [''],
+      invalid: ['a', 'ab'],
+    });
+    test({
+      validator: 'isLength',
+      args: [{ min: 5, max: 10, discreteLengths: [2, 6, 8, 9] }],
+      valid: ['helloguy', 'shopping', 'validator', 'length'],
+      invalid: ['abcde', 'abcdefg'],
+    });
+    test({
+      validator: 'isLength',
+      args: [{ discreteLengths: '9' }],
+      valid: ['a', 'abcd', 'abcdefghijkl'],
+      invalid: [],
+    });
+    test({
+      validator: 'isLength',
+      valid: ['a', '', 'asds'],
+    });
+    test({
+      validator: 'isLength',
+      args: [{ max: 8 }],
+      valid: ['👩🦰👩👩👦👦🏳️🌈', '⏩︎⏩︎⏪︎⏪︎⏭︎⏭︎⏮︎⏮︎'],
+    });
+  });
 });
