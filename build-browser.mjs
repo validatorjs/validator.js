79574a1301c132c6009534cf486aa21b2fc0539d
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2025-11-09 10:30:11 +0000

    feat(ci): update build tooling

---
+++ build-browser.mjs
@@ -0,0 +1,33 @@
+/* eslint import/no-extraneous-dependencies: 0 */
+import fs from 'fs';
+import { rollup } from 'rollup';
+import { babel } from '@rollup/plugin-babel';
+import babelPresetEnv from '@babel/preset-env';
+import pkg from "./package.json" with { type: "json" };
+
+rollup({
+  input: 'src/index.js',
+  plugins: [
+    babel({
+      presets: [[babelPresetEnv, { modules: false }]],
+      babelHelpers: 'bundled',
+      babelrc: false,
+    }),
+  ],
+})
+  .then((bundle) =>
+    bundle.write({
+      file: 'validator.js',
+      format: 'umd',
+      name: pkg.name,
+      banner: `/*!\n${String(fs.readFileSync('./LICENSE'))
+        .trim()
+        .split('\n')
+        .map((l) => ` * ${l}`)
+        .join('\n')}\n */`,
+    })
+  )
+  .catch((e) => {
+    process.stderr.write(`${e.message}\n`);
+    process.exit(1);
+  });
