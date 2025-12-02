44d030e0448bde5394ac29b05d14cbf08090f876
Sai <firewatchsai@gmail.com>
2025-12-03 04:54:32 +0530

    fix(isHexColor): add `require_hashtag` option (#2535)

--- test/validators.test.js
+++ test/validators.test.js
@@ -4997,6 +4997,48 @@ describe('Validators', () => {
         '#ff',
         'fff0a',
         '#ff12FG',
+        '#######',
+        '',
+      ],
+    });
+    test({
+      validator: 'isHexColor',
+      args: [{ require_hashtag: false }],
+      valid: [
+        '#ff0000ff',
+        '#ff0034',
+        '#CCCCCC',
+        '0f38',
+        'fff',
+        '#f00',
+      ],
+      invalid: [
+        '#ff',
+        'fff0a',
+        '#ff12FG',
+        '#######',
+        '',
+      ],
+    });
+    test({
+      validator: 'isHexColor',
+      args: [{ require_hashtag: true }],
+      valid: [
+        '#ff0000ff',
+        '#ff0034',
+        '#CCCCCC',
+        '#0f38',
+        '#fff',
+        '#f00',
+      ],
+      invalid: [
+        '#ff',
+        'fff0a',
+        '#ff12FG',
+        '0f38',
+        'fff',
+        '#######',
+        '',
       ],
     });
   });
