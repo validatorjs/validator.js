541883c56c7191e2e97b059b919035e740b7227a
shrawak <darksoulshrawak@gmail.com>
2026-03-31 23:07:34 +0545

    fix(isSlug): update charset regex to allow valid slug characters

--- src/lib/isSlug.js
+++ src/lib/isSlug.js
@@ -1,6 +1,6 @@
 import assertString from './util/assertString';
 
-let charsetRegex = /^[a-z0-9](?!.*[-_]{2,})(?:[a-z0-9-_]*[a-z0-9])?$/;
+let charsetRegex = /^[a-z0-9](?!.*[-_]{2,})(?:[a-z0-9_-]*[a-z0-9])?$/;
 
 export default function isSlug(str) {
   assertString(str);
