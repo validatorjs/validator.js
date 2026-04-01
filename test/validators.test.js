941db7fac5263cc7e0df0eba37253678f92989b0
Shrawak <55523949+Shrawak@users.noreply.github.com>
2026-04-01 23:55:34 +0545

    fix(isSlug): restrict allowed characters to valid slug charset (#2693)

--- test/validators.test.js
+++ test/validators.test.js
@@ -14211,13 +14211,15 @@ describe('Validators', () => {
     test({
       validator: 'isSlug',
       valid: [
+        'f',
+        'fo',
         'foo',
         'foo-bar',
         'foo_bar',
         'foo-bar-foo',
         'foo-bar_foo',
-        'foo-bar_foo*75-b4r-**_foo',
-        'foo-bar_foo*75-b4r-**_foo-&&',
+        'foo-75-b4r-foo',
+        'a1-b2_c3',
       ],
       invalid: [
         'not-----------slug',
@@ -14227,6 +14229,12 @@ describe('Validators', () => {
         '_not-slug',
         'not-slug_',
         'not slug',
+        'i.am.not.a.slug',
+        'slug.is.cool',
+        'foo-bar_foo*75-b4r-**_foo',
+        'foo-bar_foo*75-b4r-**_foo-&&',
+        'Foo-Bar',
+        'a:b',
       ],
     });
   });
