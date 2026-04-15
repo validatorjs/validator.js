acd27b39ae75b6e85f71b79be11caf11dd414798
Rik Smale <git@riksmale.info>
2026-04-15 22:37:33 +0200

    test: split isDivisibleBy validator tests

--- test/validators.test.js
+++ test/validators.test.js
@@ -114,22 +114,6 @@ describe('Validators', () => {
     });
   });
 
-  it('should validate that integer strings are divisible by a number', () => {
-    test({
-      validator: 'isDivisibleBy',
-      args: [2],
-      valid: ['2', '4', '100', '1000'],
-      invalid: [
-        '1',
-        '2.5',
-        '101',
-        'foo',
-        '',
-        '2020-01-06T14:31:00.135Z',
-      ],
-    });
-  });
-
   it('should validate luhn numbers', () => {
     test({
       validator: 'isLuhnNumber',
