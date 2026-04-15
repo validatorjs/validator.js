2e0a35eaaf4951846a6dbf356e355543bc739744
Rik Smale <git@riksmale.info>
2026-04-15 22:46:34 +0200

    test: split isMongoId validator tests

--- test/validators.test.js
+++ test/validators.test.js
@@ -114,21 +114,6 @@ describe('Validators', () => {
     });
   });
 
-  it('should validate hex-encoded MongoDB ObjectId', () => {
-    test({
-      validator: 'isMongoId',
-      valid: [
-        '507f1f77bcf86cd799439011',
-      ],
-      invalid: [
-        '507f1f77bcf86cd7994390',
-        '507f1f77bcf86cd79943901z',
-        '',
-        '507f1f77bcf86cd799439011 ',
-      ],
-    });
-  });
-
   it('should define the module using an AMD-compatible loader', () => {
     let window = {
       validator: null,
