b880280f1b03ffbf12fea11ff6325bc44eadb3da
Rik Smale <git@riksmale.info>
2026-04-15 22:39:24 +0200

    test: split isISIN validator tests

--- test/validators.test.js
+++ test/validators.test.js
@@ -114,28 +114,6 @@ describe('Validators', () => {
     });
   });
 
-  it('should validate ISINs', () => {
-    test({
-      validator: 'isISIN',
-      valid: [
-        'AU0000XVGZA3',
-        'DE000BAY0017',
-        'BE0003796134',
-        'SG1G55870362',
-        'GB0001411924',
-        'DE000WCH8881',
-        'PLLWBGD00016',
-        'US0378331005',
-      ],
-      invalid: [
-        'DE000BAY0018',
-        'PLLWBGD00019',
-        'foo',
-        '5398228707871528',
-      ],
-    });
-  });
-
   it('should validate EANs', () => {
     test({
       validator: 'isEAN',
