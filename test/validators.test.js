f90c41db17666a83030160190228215c0872ce1c
Rik Smale <git@riksmale.info>
2026-04-15 22:33:01 +0200

    test: split contains validator tests

--- test/validators.test.js
+++ test/validators.test.js
@@ -114,33 +114,6 @@ describe('Validators', () => {
     });
   });
 
-  it('should validate strings contain another string', () => {
-    test({
-      validator: 'contains',
-      args: ['foo'],
-      valid: ['foo', 'foobar', 'bazfoo'],
-      invalid: ['bar', 'fobar'],
-    });
-
-    test({
-      validator: 'contains',
-      args: ['foo', {
-        ignoreCase: true,
-      }],
-      valid: ['Foo', 'FOObar', 'BAZfoo'],
-      invalid: ['bar', 'fobar', 'baxoof'],
-    });
-
-    test({
-      validator: 'contains',
-      args: ['foo', {
-        minOccurrences: 2,
-      }],
-      valid: ['foofoofoo', '12foo124foo', 'fofooofoooofoooo', 'foo1foo'],
-      invalid: ['foo', 'foobar', 'Fooofoo', 'foofo'],
-    });
-  });
-
   it('should validate strings against a pattern', () => {
     test({
       validator: 'matches',
