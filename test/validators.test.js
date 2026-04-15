6711e41771d5e331c691156819cabbf1ce8b0421
Rik Smale <git@riksmale.info>
2026-04-15 22:33:24 +0200

    test: split matches validator tests

--- test/validators.test.js
+++ test/validators.test.js
@@ -114,27 +114,6 @@ describe('Validators', () => {
     });
   });
 
-  it('should validate strings against a pattern', () => {
-    test({
-      validator: 'matches',
-      args: [/abc/],
-      valid: ['abc', 'abcdef', '123abc'],
-      invalid: ['acb', 'Abc'],
-    });
-    test({
-      validator: 'matches',
-      args: ['abc'],
-      valid: ['abc', 'abcdef', '123abc'],
-      invalid: ['acb', 'Abc'],
-    });
-    test({
-      validator: 'matches',
-      args: ['abc', 'i'],
-      valid: ['abc', 'abcdef', '123abc', 'AbC'],
-      invalid: ['acb'],
-    });
-  });
-
   it('should validate isLocale codes', () => {
     test({
       validator: 'isLocale',
