b3f6406e9811d03bf9dcddb3315c466e72ba6cb9
Rik Smale <git@riksmale.info>
2026-04-15 23:02:54 +0200

    test: split isSlug validator tests

--- test/validators.test.js
+++ test/validators.test.js
@@ -28,38 +28,6 @@ describe('Validators', () => {
     assert.strictEqual(window.validator.trim('  foobar '), 'foobar');
   });
 
-  it('should validate slug', () => {
-    test({
-      validator: 'isSlug',
-      valid: [
-        'f',
-        'fo',
-        'foo',
-        'foo-bar',
-        'foo_bar',
-        'foo-bar-foo',
-        'foo-bar_foo',
-        'foo-75-b4r-foo',
-        'a1-b2_c3',
-      ],
-      invalid: [
-        'not-----------slug',
-        '@#_$@',
-        '-not-slug',
-        'not-slug-',
-        '_not-slug',
-        'not-slug_',
-        'not slug',
-        'i.am.not.a.slug',
-        'slug.is.cool',
-        'foo-bar_foo*75-b4r-**_foo',
-        'foo-bar_foo*75-b4r-**_foo-&&',
-        'Foo-Bar',
-        'a:b',
-      ],
-    });
-  });
-
   it('should validate strong passwords', () => {
     test({
       validator: 'isStrongPassword',
