8a29ed7eec11ac63f836d45ff4e38398a6339a17
Rik Smale <git@riksmale.info>
2026-04-15 22:57:08 +0200

    test: split isISO4217 validator tests

--- test/validators.test.js
+++ test/validators.test.js
@@ -28,41 +28,6 @@ describe('Validators', () => {
     assert.strictEqual(window.validator.trim('  foobar '), 'foobar');
   });
 
-  it('should validate ISO 4217 corrency codes', () => {
-    // from https://en.wikipedia.org/wiki/ISO_4217
-    test({
-      validator: 'isISO4217',
-      valid: [
-        'AED',
-        'aed',
-        'AUD',
-        'CUP',
-        'EUR',
-        'GBP',
-        'LYD',
-        'MYR',
-        'SGD',
-        'SLE',
-        'USD',
-        'VED',
-        'SLE',
-      ],
-      invalid: [
-        '',
-        '$',
-        'US',
-        'us',
-        'AAA',
-        'aaa',
-        'RWA',
-        'EURO',
-        'euro',
-        'HRK',
-        'CUC',
-      ],
-    });
-  });
-
   it('should validate whitelisted characters', () => {
     test({
       validator: 'isWhitelisted',
