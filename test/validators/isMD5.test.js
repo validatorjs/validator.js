0c2e05f374270d94bfc5bf1f1ba84374743f0d0e
Rik Smale <git@riksmale.info>
2026-04-15 22:28:13 +0200

    test: split isMD5 validator tests

---
+++ test/validators/isMD5.test.js
@@ -0,0 +1,21 @@
+import test from '../testFunctions';
+
+describe('isMD5', () => {
+  it('should validate md5 strings', () => {
+    test({
+      validator: 'isMD5',
+      valid: [
+        'd94f3f016ae679c3008de268209132f2',
+        '751adbc511ccbe8edf23d486fa4581cd',
+        '88dae00e614d8f24cfd5a8b3f8002e93',
+        '0bf1c35032a71a14c2f719e5a14c1e96',
+      ],
+      invalid: [
+        'KYT0bf1c35032a71a14c2f719e5a14c1',
+        'q94375dj93458w34',
+        '39485729348',
+        '%&FHKJFvk',
+      ],
+    });
+  });
+});
