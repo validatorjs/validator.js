0a148402dde39d63851bccbb73b03cbd2fd15c2d
Rik Smale <git@riksmale.info>
2026-04-15 22:16:41 +0200

    test: split isNumeric validator tests

--- test/validators.test.js
+++ test/validators.test.js
@@ -115,99 +115,6 @@ describe('Validators', () => {
     });
   });
 
-  it('should validate numeric strings', () => {
-    test({
-      validator: 'isNumeric',
-      valid: [
-        '123',
-        '00123',
-        '-00123',
-        '0',
-        '-0',
-        '+123',
-        '123.123',
-        '+000000',
-      ],
-      invalid: [
-        ' ',
-        '',
-        '.',
-      ],
-    });
-  });
-
-  it('should validate numeric strings without symbols', () => {
-    test({
-      validator: 'isNumeric',
-      args: [{
-        no_symbols: true,
-      }],
-      valid: [
-        '123',
-        '00123',
-        '0',
-      ],
-      invalid: [
-        '-0',
-        '+000000',
-        '',
-        '+123',
-        '123.123',
-        '-00123',
-        ' ',
-        '.',
-      ],
-    });
-  });
-
-  it('should validate numeric strings with locale', () => {
-    test({
-      validator: 'isNumeric',
-      args: [{
-        locale: 'fr-FR',
-      }],
-      valid: [
-        '123',
-        '00123',
-        '-00123',
-        '0',
-        '-0',
-        '+123',
-        '123,123',
-        '+000000',
-      ],
-      invalid: [
-        ' ',
-        '',
-        ',',
-      ],
-    });
-  });
-
-  it('should validate numeric strings with locale', () => {
-    test({
-      validator: 'isNumeric',
-      args: [{
-        locale: 'fr-CA',
-      }],
-      valid: [
-        '123',
-        '00123',
-        '-00123',
-        '0',
-        '-0',
-        '+123',
-        '123,123',
-        '+000000',
-      ],
-      invalid: [
-        ' ',
-        '',
-        '.',
-      ],
-    });
-  });
-
   it('should validate ports', () => {
     test({
       validator: 'isPort',
@@ -13082,4 +12989,3 @@ describe('Validators', () => {
     });
   });
 });
-
