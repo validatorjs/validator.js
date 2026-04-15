cdf0c52ff5b01a60ccbee12ac62467d2623f8ed4
Rik Smale <git@riksmale.info>
2026-04-15 23:06:58 +0200

    test: split isMailtoURI validator tests

--- test/validators.test.js
+++ test/validators.test.js
@@ -1,7 +1,6 @@
 import assert from 'assert';
 import fs from 'fs';
 import vm from 'vm';
-import test from './testFunctions';
 
 let validator_js = fs.readFileSync(require.resolve('../validator.js')).toString();
 
@@ -25,57 +24,5 @@ describe('Validators', () => {
     let sandbox = vm.createContext(window);
     vm.runInContext(validator_js, sandbox);
     assert.strictEqual(window.validator.trim('  foobar '), 'foobar');
-  });
-
-  it('should validate mailto URI', () => {
-    test({
-      validator: 'isMailtoURI',
-      valid: [
-        'mailto:?subject=something&cc=valid@mail.com',
-        'mailto:?subject=something&cc=valid@mail.com,another@mail.com,',
-        'mailto:?subject=something&bcc=valid@mail.com',
-        'mailto:?subject=something&bcc=valid@mail.com,another@mail.com',
-        'mailto:?bcc=valid@mail.com,another@mail.com',
-        'mailto:?cc=valid@mail.com,another@mail.com',
-        'mailto:?cc=valid@mail.com',
-        'mailto:?bcc=valid@mail.com',
-        'mailto:?subject=something&body=something else',
-        'mailto:?subject=something&body=something else&cc=hello@mail.com,another@mail.com',
-        'mailto:?subject=something&body=something else&bcc=hello@mail.com,another@mail.com',
-        'mailto:?subject=something&body=something else&cc=something@mail.com&bcc=hello@mail.com,another@mail.com',
-        'mailto:hello@mail.com',
-        'mailto:info@mail.com?',
-        'mailto:hey@mail.com?subject=something',
-        'mailto:info@mail.com?subject=something&cc=valid@mail.com',
-        'mailto:info@mail.com?subject=something&cc=valid@mail.com,another@mail.com,',
-        'mailto:info@mail.com?subject=something&bcc=valid@mail.com',
-        'mailto:info@mail.com?subject=something&bcc=valid@mail.com,another@mail.com',
-        'mailto:info@mail.com?bcc=valid@mail.com,another@mail.com',
-        'mailto:info@mail.com?cc=valid@mail.com,another@mail.com',
-        'mailto:info@mail.com?cc=valid@mail.com',
-        'mailto:info@mail.com?bcc=valid@mail.com&',
-        'mailto:info@mail.com?subject=something&body=something else',
-        'mailto:info@mail.com?subject=something&body=something else&cc=hello@mail.com,another@mail.com',
-        'mailto:info@mail.com?subject=something&body=something else&bcc=hello@mail.com,another@mail.com',
-        'mailto:info@mail.com?subject=something&body=something else&cc=something@mail.com&bcc=hello@mail.com,another@mail.com',
-        'mailto:',
-      ],
-      invalid: [
-        '',
-        'something',
-        'valid@gmail.com',
-        'mailto:?subject=okay&subject=444',
-        'mailto:?subject=something&wrong=888',
-        'mailto:somename@ｇｍａｉｌ.com',
-        'mailto:hello@world.com?cc=somename@ｇｍａｉｌ.com',
-        'mailto:hello@world.com?bcc=somename@ｇｍａｉｌ.com',
-        'mailto:hello@world.com?bcc=somename@ｇｍａｉｌ.com&bcc',
-        'mailto:valid@gmail.com?subject=anything&body=nothing&cc=&bcc=&key=',
-        'mailto:hello@world.com?cc=somename',
-        'mailto:somename',
-        'mailto:info@mail.com?subject=something&body=something else&cc=something@mail.com&bcc=hello@mail.com,another@mail.com&',
-        'mailto:?subject=something&body=something else&cc=something@mail.com&bcc=hello@mail.com,another@mail.com&',
-      ],
-    });
   });
 });
