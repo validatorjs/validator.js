67c352d0360eb6ced1ec957973dec5bfb9594ef2
Rik Smale <git@riksmale.info>
2026-04-15 22:42:16 +0200

    test: split isMultibyte validator tests

--- test/validators.test.js
+++ test/validators.test.js
@@ -114,25 +114,6 @@ describe('Validators', () => {
     });
   });
 
-  it('should validate multibyte strings', () => {
-    test({
-      validator: 'isMultibyte',
-      valid: [
-        'ひらがな・カタカナ、．漢字',
-        'あいうえお foobar',
-        'test＠example.com',
-        '1234abcDEｘｙｚ',
-        'ｶﾀｶﾅ',
-        '中文',
-      ],
-      invalid: [
-        'abc',
-        'abc123',
-        '<>@" *.',
-      ],
-    });
-  });
-
   it('should validate ascii strings', () => {
     test({
       validator: 'isAscii',
