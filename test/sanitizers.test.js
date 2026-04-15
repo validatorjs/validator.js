5f692290359b5fc7bba635784c7f8fbbb6c27203
Rik Smale <git@riksmale.info>
2026-04-15 23:15:49 +0200

    test: split rtrim sanitizer tests

--- test/sanitizers.test.js
+++ test/sanitizers.test.js
@@ -26,30 +26,6 @@ function test(options) {
 }
 
 describe('Sanitizers', () => {
-  it('should trim whitespace', () => {
-    test({
-      sanitizer: 'rtrim',
-      expect: {
-        '  \r\n\tfoo  \r\n\t   ': '  \r\n\tfoo',
-        ' \r\n  \t': '',
-      },
-    });
-  });
-
-  it('should trim custom characters', () => {
-    test({
-      sanitizer: 'rtrim',
-      args: ['01'],
-      expect: { '010100201000': '0101002' },
-    });
-
-    test({
-      sanitizer: 'rtrim',
-      args: ['\\S'],
-      expect: { '01010020100001\\S': '01010020100001' },
-    });
-  });
-
   it('should convert strings to integers', () => {
     test({
       sanitizer: 'toInt',
