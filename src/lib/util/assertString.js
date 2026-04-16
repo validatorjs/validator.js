872f4189e759365c7ba286ad26af171a5aef314f
Pascal <27069811+newtondividedbysqm@users.noreply.github.com>
2026-04-16 15:11:06 +0200

    fix unreachable nullish-coalescing from within TypeErrorConstructor

--- src/lib/util/assertString.js
+++ src/lib/util/assertString.js
@@ -1,4 +1,7 @@
 export default function assertString(input) {
   if (input === undefined || input === null) throw new TypeError(`Expected a string but received a ${input}`);
-  if (typeof input !== 'string' && !(input instanceof String)) throw new TypeError(`Expected a string but received a ${input?.constructor?.name ?? typeof input}`);
+  if (typeof input !== 'string' && !(input instanceof String)) {
+    let inputConstructorName = input?.constructor?.name ? input.constructor.name : typeof input;
+    throw new TypeError(`Expected a string but received a ${inputConstructorName}`);
+  }
 }
