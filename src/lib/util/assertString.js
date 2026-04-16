faf79661f739b3a36de6d8537aef634646da22bf
Pascal <27069811+newtondividedbysqm@users.noreply.github.com>
2026-04-16 19:18:04 +0200

    fix using ObjectToString over instanceof to prevent the check to fail when executed in a different context

--- src/lib/util/assertString.js
+++ src/lib/util/assertString.js
@@ -1,6 +1,6 @@
 export default function assertString(input) {
   if (input === undefined || input === null) throw new TypeError(`Expected a string but received a ${input}`);
-  if (typeof input !== 'string' && !(input instanceof String)) {
+  if (typeof input !== 'string' && Object.prototype.toString.call(input) !== '[object String]') {
     let inputConstructorName = input?.constructor?.name ? input.constructor.name : typeof input;
     throw new TypeError(`Expected a string but received a ${inputConstructorName}`);
   }
