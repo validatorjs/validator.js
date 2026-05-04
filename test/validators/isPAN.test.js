417412e2d98491b4ae9017952db89d1f02174650
AYUSHI1007 <ayushipandeyjamshedpur@gmail.com>
2026-05-04 11:17:09 +0530

    Node 8 fix

--- test/validators/isPAN.test.js
+++ test/validators/isPAN.test.js
@@ -1,23 +1,24 @@
 import validator from '../../src/index';
+import assert from 'assert';
 
 describe('isPAN', () => {
   it('valid PAN', () => {
-    validator.isPAN('ABCDE1234F').should.equal(true);
+    assert.strictEqual(validator.isPAN('ABCDE1234F'), true);
   });
 
   it('invalid length', () => {
-    validator.isPAN('ABCDE123F').should.equal(false);
+    assert.strictEqual(validator.isPAN('ABCDE123F'), false);
   });
 
   it('lowercase should fail', () => {
-    validator.isPAN('abcde1234f').should.equal(false);
+    assert.strictEqual(validator.isPAN('abcde1234f'), false);
   });
 
   it('wrong format', () => {
-    validator.isPAN('1234ABCDE1').should.equal(false);
+    assert.strictEqual(validator.isPAN('1234ABCDE1'), false);
   });
 
   it('empty string', () => {
-    validator.isPAN('').should.equal(false);
+    assert.strictEqual(validator.isPAN(''), false);
   });
 });
